b4f70d76ae629155f855cbe56e5bf7c76da229c2
harlam357 <harlam357@8030a5b2-31bd-11de-9e80-b52b74bdfc52>
2010-07-31 19:43:52 +0000

    - Add: ProjectDownloadView and Form implementation.

---
+++ trunk/HFM/Forms/frmProjects.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace HFM.Forms
+{
+   partial class frmProjects
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            _timer.Dispose();
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProjects));
+         this.progressBar1 = new System.Windows.Forms.ProgressBar();
+         this.lblProject = new HFM.Classes.LabelWrapper();
+         this.SuspendLayout();
+         // 
+         // progressBar1
+         // 
+         this.progressBar1.Location = new System.Drawing.Point(12, 12);
+         this.progressBar1.Name = "progressBar1";
+         this.progressBar1.Size = new System.Drawing.Size(306, 24);
+         this.progressBar1.TabIndex = 0;
+         // 
+         // lblProject
+         // 
+         this.lblProject.Location = new System.Drawing.Point(12, 44);
+         this.lblProject.Name = "lblProject";
+         this.lblProject.Size = new System.Drawing.Size(306, 13);
+         this.lblProject.TabIndex = 1;
+         this.lblProject.Text = "lblProject";
+         this.lblProject.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+         // 
+         // frmProjects
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(330, 68);
+         this.Controls.Add(this.lblProject);
+         this.Controls.Add(this.progressBar1);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+         this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+         this.MaximizeBox = false;
+         this.MinimizeBox = false;
+         this.Name = "frmProjects";
+         this.ShowInTaskbar = false;
+         this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+         this.Text = "Project Download";
+         this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmProjects_FormClosing);
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.ProgressBar progressBar1;
+      private HFM.Classes.LabelWrapper lblProject;
+   }
+}
---
+++ trunk/HFM/Forms/frmProjects.cs
@@ -0,0 +1,125 @@
+﻿/*
+ * HFM.NET - Project Download Form Class
+ * Copyright (C) 2010 Ryan Harlamert (harlam357)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License. See the included file GPLv2.TXT.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Windows.Forms;
+
+using HFM.Framework;
+
+namespace HFM.Forms
+{
+   // ReSharper disable InconsistentNaming
+   public partial class frmProjects : Classes.FormWrapper, IProjectDownloadView
+   // ReSharper restore InconsistentNaming
+   {
+      private readonly frmMain _mainView;
+      private readonly IProteinCollection _proteinCollection;
+
+      private readonly System.Timers.Timer _timer;
+
+      public frmProjects(frmMain mainView, IProteinCollection proteinCollection)
+      {
+         _mainView = mainView;
+         _proteinCollection = proteinCollection;
+
+         _timer = new System.Timers.Timer(2000);
+         _timer.Elapsed += TimerElapsed;
+         
+         InitializeComponent();
+      }
+
+      #region IProjectDownloadView Members
+
+      public void Download()
+      {
+         UpdateText(_proteinCollection.Downloader.Prefs.GetPreference<string>(Preference.ProjectDownloadUrl));
+         _proteinCollection.Downloader.DownloadProgress += DownloaderProgress;
+         _proteinCollection.Downloader.ProjectDownloadFinished += DownloaderProjectDownloadFinished;
+         _proteinCollection.BeginDownloadFromStanford();
+         ShowDialog(_mainView);
+      }
+
+      private void DownloaderProgress(object sender, DownloadProgressEventArgs e)
+      {
+         UpdateProgress(e.Progress);
+         UpdateText(e.ProtienName);
+      }
+
+      private void DownloaderProjectDownloadFinished(object sender, EventArgs e)
+      {
+         _proteinCollection.Downloader.DownloadProgress -= DownloaderProgress;
+         _proteinCollection.Downloader.ProjectDownloadFinished -= DownloaderProjectDownloadFinished;
+
+         DoDelayedClose();
+      }
+
+      #endregion
+      
+      private void DoDelayedClose()
+      {
+         if (Visible)
+         {
+            _timer.Start();
+         }
+      }
+
+      private void TimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+      {
+         CloseView();
+      }
+
+      private void CloseView()
+      {
+         if (InvokeRequired)
+         {
+            Invoke(new MethodInvoker(Close));
+            return;
+         }
+
+         Close();
+      }
+
+      private void frmProjects_FormClosing(object sender, FormClosingEventArgs e)
+      {
+         _timer.Stop();
+      }
+      
+      private void UpdateProgress(int value)
+      {
+         if (InvokeRequired)
+         {
+            Invoke(new MethodInvoker(() => UpdateProgress(value)));
+            return;
+         }
+
+         progressBar1.Value = value;
+      }
+      
+      private void UpdateText(string text)
+      {
+         if (InvokeRequired)
+         {
+            Invoke(new MethodInvoker(() => UpdateText(text)));
+            return;
+         }
+
+         lblProject.Text = text; 
+      }
+   }
+}
