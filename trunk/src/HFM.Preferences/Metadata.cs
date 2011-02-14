5c9de7303df488b4f090dc04fdaecac8474abd52
harlam357 <harlam357@8030a5b2-31bd-11de-9e80-b52b74bdfc52>
2011-02-14 01:33:33 +0000

    Add HFM.Log, HFM.Plugins, HFM.Preferences, and HFM.Proteins Projects.

---
+++ trunk/src/HFM.Preferences/Metadata.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * HFM.NET - Generic Metadata Class and Interface
+ * Copyright (C) 2009-2010 Ryan Harlamert (harlam357)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License. See the included file GPLv2.TXT.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+ 
+/* 
+ * Based on code by Bryan Watts
+ * http://stackoverflow.com/questions/353126/c-multiple-generic-types-in-one-list/1351071
+ */
+
+using System;
+
+namespace HFM.Preferences
+{
+   public interface IMetadata
+   {
+      Type DataType { get; }
+
+      object Data { get; }
+   }
+
+   public interface IMetadata<T> : IMetadata
+   {
+      new T Data { get; }
+   }
+
+   public class Metadata<T> : IMetadata<T>
+   {
+      public Metadata()
+      {
+         Data = default(T);
+      }
+
+      public Metadata(T data)
+      {
+         Data = data;
+      }
+
+      public Type DataType
+      {
+         get { return typeof(T); }
+      }
+
+      object IMetadata.Data
+      {
+         get { return Data; }
+      }
+
+      public T Data { get; set; }
+   }
+}
