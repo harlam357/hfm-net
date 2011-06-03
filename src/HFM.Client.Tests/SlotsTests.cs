573b0dcb27910cbf28ff9f6469c51e9b41256f1c
harlam357 <harlam357@8030a5b2-31bd-11de-9e80-b52b74bdfc52>
2011-06-03 05:34:17 +0000

    - Updates to Slots and Options parsing. - Add HFM.Client.Tests unit test project and test files.

---
+++ src/HFM.Client.Tests/SlotsTests.cs
@@ -0,0 +1,19 @@
+﻿
+using System.IO;
+
+using NUnit.Framework;
+
+namespace HFM.Client.Tests
+{
+   [TestFixture]
+   public class SlotsTests
+   {
+      [Test]
+      public void ParseTest1()
+      {
+         string message = File.ReadAllText("..\\..\\..\\TestFiles\\Client_v7_1\\slots.txt");
+         var slots = Slots.Parse(Messages.GetNextMessage(ref message));
+         Assert.AreEqual(1, slots.Count);
+      }
+   }
+}
