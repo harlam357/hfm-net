b30a86cf9b5a3e03e4c0fc8f638ec91ea2dae596
harlam357 <harlam357@8030a5b2-31bd-11de-9e80-b52b74bdfc52>
2011-06-05 06:53:01 +0000

    - Add Heartbeat type and tests.

--- trunk/src/HFM.Client.Tests/QueueTests.cs
+++ trunk/src/HFM.Client.Tests/QueueTests.cs
@@ -17,6 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System;
 using System.IO;
 
 using NUnit.Framework;
@@ -31,6 +32,31 @@ namespace HFM.Client.Tests
       {
          string message = File.ReadAllText("..\\..\\..\\TestFiles\\Client_v7_1\\units.txt");
          var queue = Queue.Parse(Messages.GetNextMessage(ref message));
+         Assert.AreEqual(0, queue[0].ID);
+         Assert.AreEqual("RUNNING", queue[0].State);
+         Assert.AreEqual(11020, queue[0].Project);
+         Assert.AreEqual(0, queue[0].Run);
+         Assert.AreEqual(1921, queue[0].Clone);
+         Assert.AreEqual(24, queue[0].Gen);
+         Assert.AreEqual("0xa3", queue[0].Core);
+         Assert.AreEqual("0x000000210a3b1e5b4d824701aee79f1e", queue[0].Unit);
+         Assert.AreEqual("59.00%", queue[0].PercentDone);
+         Assert.AreEqual(1000, queue[0].TotalFrames);
+         Assert.AreEqual(590, queue[0].FramesDone);
+         Assert.AreEqual("27/May/2011-19:34:24", queue[0].Assigned);
+         Assert.AreEqual("04/Jun/2011-19:34:24", queue[0].Timeout);
+         Assert.AreEqual("08/Jun/2011-19:34:24", queue[0].Deadline);
+         Assert.AreEqual("171.64.65.55", queue[0].WorkServer);
+         Assert.AreEqual("171.67.108.26", queue[0].CollectionServer);
+         Assert.AreEqual(String.Empty, queue[0].WaitingOn);
+         Assert.AreEqual(0, queue[0].Attempts);
+         Assert.AreEqual("0.00 secs", queue[0].NextAttempt);
+         Assert.AreEqual(0, queue[0].Slot);
+         Assert.AreEqual("2 hours 28 mins", queue[0].ETA);
+         Assert.AreEqual(1749.96, queue[0].PPD);
+         Assert.AreEqual("3 mins 38 secs", queue[0].TPF);
+         Assert.AreEqual(443, queue[0].BaseCredit);
+         Assert.AreEqual(443, queue[0].CreditEstimate);
       }
    }
 }
