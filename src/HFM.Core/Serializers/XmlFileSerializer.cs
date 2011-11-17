553c9377619a4563ee6653554b9e2a7eef260a4d
harlam357 <harlam357@8030a5b2-31bd-11de-9e80-b52b74bdfc52>
2011-11-17 00:47:12 +0000

    - Add: HFM.Core.Plugins & HFM.Core/Serializers.

---
+++ src/HFM.Core/Serializers/XmlFileSerializer.cs
@@ -0,0 +1,43 @@
+﻿
+using System.IO;
+using System.Runtime.Serialization;
+
+using HFM.Plugins;
+
+namespace HFM.Core.Serializers
+{
+   public class XmlFileSerializer<T> : IFileSerializer<T> where T : class, new()
+   {
+      #region IProteinBenchmarkSerializer Members
+
+      public string FileExtension
+      {
+         get { return "xml"; }
+      }
+
+      public string FileTypeFilter
+      {
+         get { return "Xml Files|*.xml"; }
+      }
+
+      public T Deserialize(string fileName)
+      {
+         using (var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+         {
+            var serializer = new DataContractSerializer(typeof(T));
+            return (T)serializer.ReadObject(fileStream);
+         }
+      }
+
+      public void Serialize(string fileName, T value)
+      {
+         using (var fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+         {
+            var serializer = new DataContractSerializer(typeof(T));
+            serializer.WriteObject(fileStream, value);
+         }
+      }
+
+      #endregion
+   }
+}
