5f458741f1c9fb6153f9bbe7e5495a7f0f142c22
harlam357 <harlam357@8030a5b2-31bd-11de-9e80-b52b74bdfc52>
2011-11-17 02:58:25 +0000

    - Change: AssemblyVersion 1.0.0.0 to accommodate strong naming for plugins. - Add: HFM.Proteins.ExampleSerializer Project.

---
+++ trunk/src/HFM.Proteins.ExampleSerializer/Plugin.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using HFM.Core.DataTypes;
+using HFM.Core.Plugins;
+
+namespace HFM.Proteins.ExampleSerializer
+{
+   public class Plugin : IFileSerializer<List<Protein>>
+   {
+      public string FileExtension
+      {
+         get { return "xml"; }
+         //get { return null; }
+      }
+
+      /// <summary>
+      /// File Type Filter for File Dialogs (example "Xml Files|*.xml")
+      /// </summary>
+      public string FileTypeFilter
+      {
+         get { return "Xml Files|*.xml"; }
+      }
+
+      public List<Protein> Deserialize(string fileName)
+      {
+         throw new NotImplementedException();
+      }
+
+      public void Serialize(string fileName, List<Protein> values)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
