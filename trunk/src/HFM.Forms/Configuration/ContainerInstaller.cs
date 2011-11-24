3123e82bf2c8dd666f5f74cb421c670dee906c06
harlam357 <harlam357@8030a5b2-31bd-11de-9e80-b52b74bdfc52>
2011-11-24 01:17:20 +0000

    - Update: HFM solution files. - Add: ContainerInstaller.cs from previous commit.

---
+++ trunk/src/HFM.Forms/Configuration/ContainerInstaller.cs
@@ -0,0 +1,141 @@
+﻿/*
+ * HFM.NET - Forms Container Installer
+ * Copyright (C) 2009-2011 Ryan Harlamert (harlam357)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License. See the included file GPLv2.TXT.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+using harlam357.Windows.Forms;
+
+using HFM.Core;
+
+namespace HFM.Forms.Configuration
+{
+   [CoverageExclude]
+   public class ContainerInstaller : IWindsorInstaller
+   {
+      #region IWindsorInstaller Members
+
+      public void Install(IWindsorContainer container, IConfigurationStore store)
+      {
+         #region Data Models
+
+         // UserStatsDataModel - Singleton
+         container.Register(
+            Component.For<Models.UserStatsDataModel>());
+
+         #endregion
+
+         #region View Interfaces
+
+         // IMessageBoxView - Transient
+         container.Register(
+            Component.For<IMessageBoxView>()
+               .ImplementedBy<MessageBoxView>()
+               .LifeStyle.Transient);
+
+         // IOpenFileDialogView - Transient
+         container.Register(
+            Component.For<IOpenFileDialogView>()
+               .ImplementedBy<OpenFileDialogView>()
+               .LifeStyle.Transient);
+
+         // ISaveFileDialogView - Transient
+         container.Register(
+            Component.For<ISaveFileDialogView>()
+               .ImplementedBy<SaveFileDialogView>()
+               .LifeStyle.Transient);
+
+         // IFolderBrowserView - Transient
+         container.Register(
+            Component.For<IFolderBrowserView>()
+               .ImplementedBy<FolderBrowserView>()
+               .LifeStyle.Transient);
+
+         // IBenchmarksView - Transient
+         container.Register(
+            Component.For<IBenchmarksView>()
+               .ImplementedBy<BenchmarksForm>()
+               .LifeStyle.Transient);
+
+         // IHistoryView - Transient
+         container.Register(
+            Component.For<IHistoryView>()
+               .ImplementedBy<HistoryForm>()
+               .LifeStyle.Transient);
+
+         // ILegacyClientSetupView - Transient
+         container.Register(
+            Component.For<ILegacyClientSetupView>()
+               .ImplementedBy<LegacyClientSetupDialog>()
+               .LifeStyle.Transient);
+
+         // IMainView - Singleton
+         container.Register(
+            Component.For<IMainView>()
+               .ImplementedBy<frmMain>());
+
+         // IMainView - Singleton
+         container.Register(
+            Component.For<IMessagesView>()
+               .ImplementedBy<MessagesForm>());
+
+         // IQueryView - Transient
+         container.Register(
+            Component.For<IQueryView>()
+               .ImplementedBy<QueryDialog>()
+               .LifeStyle.Transient);
+
+         #endregion
+
+         #region Presenters
+
+         // MainPresenter - Singleton
+         container.Register(
+            Component.For<MainPresenter>());
+
+         #endregion
+
+         #region Service Interfaces
+
+         // IAutoRun - Singleton
+         container.Register(
+            Component.For<IAutoRun>()
+               .ImplementedBy<AutoRun>());
+
+         // IUpdateLogic - Singleton
+         container.Register(
+            Component.For<IUpdateLogic>()
+               .ImplementedBy<UpdateLogic>());
+
+         #endregion
+
+         // PreferencesDialog - Transient
+         container.Register(
+            Component.For<PreferencesDialog>()
+               .LifeStyle.Transient);
+
+         // RetrievalLogic - Singleton
+         container.Register(
+            Component.For<RetrievalLogic>());
+      }
+
+      #endregion
+   }
+}
