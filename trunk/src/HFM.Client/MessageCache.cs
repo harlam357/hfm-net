cc64bd13071abc35235a8e5d40bc841eb8dd44e8
harlam357 <harlam357@8030a5b2-31bd-11de-9e80-b52b74bdfc52>
2012-03-17 22:20:46 +0000

    - Update the XML Documentation for HFM.Client API in preparation for release.

--- trunk/src/HFM.Client/MessageCache.cs
+++ trunk/src/HFM.Client/MessageCache.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * HFM.NET - MessageCache Class
- * Copyright (C) 2009-2011 Ryan Harlamert (harlam357)
+ * Copyright (C) 2009-2012 Ryan Harlamert (harlam357)
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -27,16 +27,19 @@ using HFM.Client.DataTypes;
 
 namespace HFM.Client
 {
+   /// <summary>
+   /// Folding@Home client message cache class.  Provides functionality for parsing, storing, and accessing the raw JSON messages returned by the Folding@Home client.
+   /// </summary>
    public class MessageCache : Connection
    {
       #region Events
 
       /// <summary>
-      /// Fired when a new or updated message is received.
+      /// Occurs when an updated message is received.
       /// </summary>
       public event EventHandler<MessageUpdatedEventArgs> MessageUpdated;
       /// <summary>
-      /// Fired when a data update is complete.
+      /// Occurs when a local data buffer update is finished.
       /// </summary>
       public event EventHandler UpdateFinished;
       
@@ -61,7 +64,7 @@ namespace HFM.Client
       #region Constructor
 
       /// <summary>
-      /// Create a Server Message Cache.
+      /// Initializes a new instance of the MessageCache class.
       /// </summary>
       public MessageCache()
          : this(new TcpClientFactory())
@@ -70,7 +73,7 @@ namespace HFM.Client
       }
 
       /// <summary>
-      /// Create a Server Message Cache.
+      /// Initializes a new instance of the MessageCache class.
       /// </summary>
       internal MessageCache(ITcpClientFactory tcpClientFactory)
          : base(tcpClientFactory)
@@ -84,16 +87,16 @@ namespace HFM.Client
       #region Methods
 
       /// <summary>
-      /// Get a Server Message.  Returns null if the message is not in the cache.
+      /// Get a Folding@Home client message as a JSON value.  Returns null if the requested message is not available in the cache.
       /// </summary>
-      /// <param name="key">Server Message Key</param>
+      /// <param name="key">JSON message key.</param>
       public JsonMessage GetJsonMessage(string key)
       {
          return _messages.ContainsKey(key) ? _messages[key] : null;
       }
 
       /// <summary>
-      /// Update the Data Buffer.
+      /// Update the local data buffer with data from the remote network stream.
       /// </summary>
       protected override void Update()
       {
@@ -117,10 +120,10 @@ namespace HFM.Client
       }
 
       /// <summary>
-      /// Parse first Message from the incoming data buffer.  Remaining buffer value is returned to the caller.
+      /// Parse first message from the data buffer and remove it from the buffer value.  The remaining buffer value is returned to the caller.
       /// </summary>
-      /// <param name="buffer">Data Buffer Value</param>
-      /// <returns>Message or null if no message is available in the buffer.</returns>
+      /// <param name="buffer">Data buffer value.</param>
+      /// <returns>JsonMessage or null if no message is available in the buffer.</returns>
       internal static JsonMessage GetNextJsonMessage(ref string buffer)
       {
          if (buffer == null) return null;
@@ -200,9 +203,9 @@ namespace HFM.Client
       }
 
       /// <summary>
-      /// Raise the MessageUpdated Event.
+      /// Raise the MessageUpdated event.
       /// </summary>
-      /// <param name="e">Event Arguments (if null the event is cancelled)</param>
+      /// <param name="e">Event arguments (if null the event is cancelled).</param>
       protected virtual void OnMessageUpdated(MessageUpdatedEventArgs e)
       {
          if (e == null) return;
@@ -224,62 +227,71 @@ namespace HFM.Client
       #endregion
    }
 
+   /// <summary>
+   /// Provides data for message updated events of a MessageCache connection.
+   /// </summary>
    public class MessageUpdatedEventArgs : EventArgs
    {
       /// <summary>
-      /// Messgae key that was updated.
+      /// Gets the message key that was updated.
       /// </summary>
       public string Key { get; private set; }
       /// <summary>
-      /// Messgae data type that was updated.
+      /// Gets the message data type that was updated (FahClient connections only).
       /// </summary>
       public Type DataType { get; internal set; }
 
+      /// <summary>
+      /// Initializes a new instance of the MessageUpdatedEventArgs class.
+      /// </summary>
       public MessageUpdatedEventArgs(string key)
       {
          Key = key;
       }
    }
 
+   /// <summary>
+   /// Folding@Home client message keys for JSON messages.
+   /// </summary>
    public static class JsonMessageKey
    {
       /// <summary>
-      /// Heartbeat Message Key
+      /// Heartbeat Message Key.
       /// </summary>
       public const string Heartbeat = "heartbeat";
       /// <summary>
-      /// Info Message Key
+      /// Info Message Key.
       /// </summary>
       public const string Info = "info";
       /// <summary>
-      /// Options Message Key
+      /// Options Message Key.
       /// </summary>
       public const string Options = "options";
       /// <summary>
-      /// Simulation Info Message Key
+      /// Simulation Info Message Key.
       /// </summary>
       /// <remarks>This message is in response to a command that takes a slot id argument.</remarks>
       public const string SimulationInfo = "simulation-info";
       /// <summary>
-      /// Slot Info Message Key
+      /// Slot Info Message Key.
       /// </summary>
       public const string SlotInfo = "slots";
       /// <summary>
-      /// Slot Options Message Key
+      /// Slot Options Message Key.
       /// </summary>
       /// <remarks>This message is in response to a command that takes a slot id argument.</remarks>
       public const string SlotOptions = "slot-options";
       /// <summary>
-      /// Queue Info Message Key
+      /// Queue Info Message Key.
       /// </summary>
       public const string QueueInfo = "units";
 
       /// <summary>
-      /// Log Restart Message Key
+      /// Log Restart Message Key.
       /// </summary>
       public const string LogRestart = "log-restart";
       /// <summary>
-      /// Log Update Message Key
+      /// Log Update Message Key.
       /// </summary>
       public const string LogUpdate = "log-update";
    }
