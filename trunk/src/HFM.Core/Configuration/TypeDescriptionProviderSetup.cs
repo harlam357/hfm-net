2d184abda4427ae0fd92e6fb686203baaa207421
harlam357 <harlam357@8030a5b2-31bd-11de-9e80-b52b74bdfc52>
2013-02-08 03:03:31 +0000

    - Add: TypeDescriptionProviderSetup and include Hyper and PetaPoco folders.

---
+++ trunk/src/HFM.Core/Configuration/TypeDescriptionProviderSetup.cs
@@ -0,0 +1,13 @@
+﻿
+namespace HFM.Core.Configuration
+{
+   public static class TypeDescriptionProviderSetup
+   {
+      [CoverageExclude]
+      public static void Execute()
+      {
+         Hyper.ComponentModel.HyperTypeDescriptionProvider.Add(typeof(SlotModel));
+         Hyper.ComponentModel.HyperTypeDescriptionProvider.Add(typeof(DataTypes.HistoryEntry));
+      }
+   }
+}
