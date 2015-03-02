13492dd87e914622b9dde74b88d18569a472fa30
harlam357 <harlam357@8030a5b2-31bd-11de-9e80-b52b74bdfc52>
2015-03-02 00:28:47 +0000

    - Update Queue.Tool copyright year

--- src/HFM.Queue.Tool/Program.cs
+++ src/HFM.Queue.Tool/Program.cs
@@ -1,4 +1,22 @@
-﻿
+﻿/*
+ * HFM.NET - Queue.Tool
+ * Copyright (C) 2009-2015 Ryan Harlamert (harlam357)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License. See the included file GPLv2.TXT.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -18,7 +36,7 @@ namespace HFM.Queue.Tool
          Console.WriteLine("{0} version {1} (qd clone)", app, fi.FileVersion);
          Console.WriteLine("Copyright (C) 2002-2005 Richard P. Howell IV.");
          Console.WriteLine("Copyright (C) 2005-2008 Sebastiaan Couwenberg");
-         Console.WriteLine("Copyright (C) 2009-2011 Ryan Harlamert");
+         Console.WriteLine("Copyright (C) 2009-2015 Ryan Harlamert");
          
          IEnumerable<Argument> arguments;
          try
