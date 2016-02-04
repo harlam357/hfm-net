a4f70df66aac75454effd15d609a2186a83fa28e
harlam357 <harlam357@gmail.com>
2016-02-03 23:03:20 -0600

    Rename MessageCache => JsonMessageConnection.  Rename FahClient => TypedMessageConnection.

---
+++ src/HFM.Client/JsonMessageConnection.cs
@@ -0,0 +1,316 @@
+﻿/*
+ * HFM.NET - JsonMessageConnection Class
+ * Copyright (C) 2009-2016 Ryan Harlamert (harlam357)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License. See the included file GPLv2.TXT.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+
+using HFM.Client.DataTypes;
+using HFM.Core.DataTypes;
+
+namespace HFM.Client
+{
+   /// <summary>
+   /// Folding@Home client JSON message connection class.  Provides functionality for parsing raw network data into JSON messages.
+   /// </summary>
+   public class JsonMessageConnection : Connection
+   {
+      #region Events
+
+      /// <summary>
+      /// Occurs when a message is received.
+      /// </summary>
+      public event EventHandler<MessageReceivedEventArgs> MessageReceived;
+      /// <summary>
+      /// Occurs when the local data buffer update is finished.
+      /// </summary>
+      public event EventHandler UpdateFinished;
+      
+      #endregion
+
+      #region Constants
+
+      private const string LineFeed = "\n";
+      private const string CarriageReturnLineFeed = "\r\n";
+      private const string PyonHeader = "PyON 1 ";
+      private const string PyonFooter = "---";
+
+      #endregion
+
+      #region Fields
+
+      private readonly StringBuilder _readBuffer;
+
+      private readonly object _bufferLock = new object();
+
+      #endregion
+
+      #region Constructor
+
+      /// <summary>
+      /// Initializes a new instance of the MessageCache class.
+      /// </summary>
+      [CoverageExclude]
+      public JsonMessageConnection()
+         : this(new TcpClientFactory())
+      {
+
+      }
+
+      /// <summary>
+      /// Initializes a new instance of the MessageCache class.
+      /// </summary>
+      internal JsonMessageConnection(ITcpClientFactory tcpClientFactory)
+         : base(tcpClientFactory)
+      {
+         _readBuffer = new StringBuilder();
+      }
+
+      #endregion
+
+      #region Methods
+
+      /// <summary>
+      /// Update the local data buffer with data from the remote network stream.
+      /// </summary>
+      protected override void ProcessData(string buffer, int totalBytesRead)
+      {
+         base.ProcessData(buffer, totalBytesRead);
+
+         lock (_bufferLock)
+         {
+            _readBuffer.Append(buffer);
+
+            JsonMessage json;
+            while ((json = GetNextJsonMessage(_readBuffer)) != null)
+            {
+               OnStatusMessage(new StatusMessageEventArgs(String.Format(CultureInfo.CurrentCulture,
+                  "Received message: {0} ({1} bytes)", json.Key, json.Value.Length), TraceLevel.Info));
+               OnMessageReceived(new MessageReceivedEventArgs(json));
+            }
+         }
+         // send update finished event
+         OnUpdateFinished(EventArgs.Empty);
+      }
+
+      /// <summary>
+      /// Parse first message from the data buffer and remove it from the buffer value.  The remaining buffer value is returned to the caller.
+      /// </summary>
+      /// <param name="buffer">Data buffer value.</param>
+      /// <returns>JsonMessage or null if no message is available in the buffer.</returns>
+      internal static JsonMessage GetNextJsonMessage(ref string buffer)
+      {
+         var sb = new StringBuilder(buffer);
+         var message = GetNextJsonMessage(sb);
+         buffer = sb.ToString();
+         return message;
+      }
+
+      /// <summary>
+      /// Parse first message from the data buffer and remove it from the buffer value.  The remaining buffer value is returned to the caller.
+      /// </summary>
+      /// <param name="buffer">Data buffer value.</param>
+      /// <returns>JsonMessage or null if no message is available in the buffer.</returns>
+      internal static JsonMessage GetNextJsonMessage(StringBuilder buffer)
+      {
+         if (buffer == null) return null;
+
+         // find the header
+         int messageIndex = buffer.IndexOf(PyonHeader, false);
+         if (messageIndex < 0)
+         {
+            return null;
+         }
+         // set starting message index
+         messageIndex += PyonHeader.Length;
+
+         // find the first CrLf or Lf character after the header
+         int startIndex = FindStartIndex(buffer, messageIndex);
+         if (startIndex < 0) return null;
+
+         // find the footer
+         int endIndex = FindEndIndex(buffer, startIndex);
+         if (endIndex < 0)
+         {
+            return null;
+         }
+
+         // create the message and set received time stamp
+         var message = new JsonMessage { Received = DateTime.UtcNow };
+         // get the message name
+         message.Key = buffer.Substring(messageIndex, startIndex - messageIndex);
+
+         // get the PyON message
+         buffer.SubstringBuilder(startIndex, message.Value, endIndex - startIndex);
+         // replace PyON values with JSON values
+         message.Value.Replace(": None", ": null");
+         message.Value.Replace(": True", ": true");
+         message.Value.Replace(": False", ": false");
+
+         // set the index so we know where to trim the string (end plus footer length)
+         int nextStartIndex = endIndex + PyonFooter.Length;
+         // if more buffer is available set it and return, otherwise set the buffer empty
+         if (nextStartIndex < buffer.Length)
+         {
+            buffer.Remove(0, nextStartIndex);
+         }
+         else
+         {
+            buffer.Clear();
+         }
+
+         return message;
+      }
+
+      private static int FindStartIndex(StringBuilder buffer, int messageIndex)
+      {
+         int index = buffer.IndexOf(CarriageReturnLineFeed, messageIndex, false);
+         return index >= 0 ? index : buffer.IndexOf(LineFeed, messageIndex, false);
+      }
+
+      private static int FindEndIndex(StringBuilder buffer, int startIndex)
+      {
+         int index = buffer.IndexOf(String.Concat(CarriageReturnLineFeed, PyonFooter, CarriageReturnLineFeed), startIndex, false);
+         if (index >= 0)
+         {
+            return index;
+         }
+
+         index = buffer.IndexOf(String.Concat(LineFeed, PyonFooter, LineFeed), startIndex, false);
+         if (index >= 0)
+         {
+            return index;
+         }
+
+         //index = buffer.IndexOf(PyonFooter, startIndex, StringComparison.Ordinal);
+         //if (index >= 0)
+         //{
+         //   return index;
+         //}
+
+         return -1;
+      }
+
+      /// <summary>
+      /// Raise the MessageUpdated event.
+      /// </summary>
+      /// <param name="e">Event arguments (if null the event is cancelled).</param>
+      protected virtual void OnMessageReceived(MessageReceivedEventArgs e)
+      {
+         if (e == null) return;
+
+         var handler = MessageReceived;
+         if (handler != null)
+         {
+            handler(this, e);
+         }
+      }
+
+      private void OnUpdateFinished(EventArgs e)
+      {
+         var handler = UpdateFinished;
+         if (handler != null)
+         {
+            handler(this, e);
+         }
+      }
+
+      #endregion
+   }
+
+   /// <summary>
+   /// Provides data for message received events.
+   /// </summary>
+   public class MessageReceivedEventArgs : EventArgs
+   {
+      /// <summary>
+      /// Gets the JSON message that was received.
+      /// </summary>
+      public JsonMessage JsonMessage { get; private set; }
+
+      /// <summary>
+      /// Gets the typed message that was received (FahClient connections only).
+      /// </summary>
+      public TypedMessage TypedMessage { get; internal set; }
+
+      /// <summary>
+      /// Gets the data type of the typed message that was received (FahClient connections only).
+      /// </summary>
+      public Type DataType
+      {
+         get { return TypedMessage != null ? TypedMessage.GetType() : null; }
+      }
+
+      /// <summary>
+      /// Initializes a new instance of the MessageUpdatedEventArgs class.
+      /// </summary>
+      /// <param name="jsonMessage">The JSON message.</param>
+      public MessageReceivedEventArgs(JsonMessage jsonMessage)
+      {
+         JsonMessage = jsonMessage;
+      }
+   }
+
+   /// <summary>
+   /// Folding@Home client message keys for JSON messages.
+   /// </summary>
+   public static class JsonMessageKey
+   {
+      /// <summary>
+      /// Heartbeat Message Key.
+      /// </summary>
+      public const string Heartbeat = "heartbeat";
+      /// <summary>
+      /// Info Message Key.
+      /// </summary>
+      public const string Info = "info";
+      /// <summary>
+      /// Options Message Key.
+      /// </summary>
+      public const string Options = "options";
+      /// <summary>
+      /// Simulation Info Message Key.
+      /// </summary>
+      /// <remarks>This message is in response to a command that takes a slot id argument.</remarks>
+      public const string SimulationInfo = "simulation-info";
+      /// <summary>
+      /// Slot Info Message Key.
+      /// </summary>
+      public const string SlotInfo = "slots";
+      /// <summary>
+      /// Slot Options Message Key.
+      /// </summary>
+      /// <remarks>This message is in response to a command that takes a slot id argument.</remarks>
+      public const string SlotOptions = "slot-options";
+      /// <summary>
+      /// Queue Info Message Key.
+      /// </summary>
+      public const string QueueInfo = "units";
+
+      /// <summary>
+      /// Log Restart Message Key.
+      /// </summary>
+      public const string LogRestart = "log-restart";
+      /// <summary>
+      /// Log Update Message Key.
+      /// </summary>
+      public const string LogUpdate = "log-update";
+   }
+}
---
+++ src/HFM.Client/TypedMessageConnection.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * HFM.NET - TypedMessageConnection Class
+ * Copyright (C) 2009-2016 Ryan Harlamert (harlam357)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License. See the included file GPLv2.TXT.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+
+using HFM.Client.DataTypes;
+
+namespace HFM.Client
+{
+   // TODO: Split this interface up and document
+   public interface IFahClient : IDisposable
+   {
+      #region Properties
+
+      bool Connected { get; }
+
+      int ConnectTimeout { get; set; }
+
+      int SendBufferSize { get; set; }
+
+      int ReceiveBufferSize { get; set; }
+
+      #endregion
+
+      #region Events
+
+      event EventHandler<ConnectedChangedEventArgs> ConnectedChanged;
+      event EventHandler<DataEventArgs> DataReceived;
+      event EventHandler<DataEventArgs> DataSent;
+      event EventHandler<MessageReceivedEventArgs> MessageReceived;
+      event EventHandler<StatusMessageEventArgs> StatusMessage;
+      event EventHandler UpdateFinished;
+
+      #endregion
+
+      #region Methods
+
+      void Connect(string host, int port, string password);
+
+      void Close();
+
+      void SendCommand(string command);
+
+      #endregion
+   }
+
+   /// <summary>
+   /// Folding@Home client typed message connection class.  Provides functionality for parsing JSON messages into strongly typed objects.
+   /// </summary>
+   public class TypedMessageConnection : JsonMessageConnection, IFahClient
+   {
+      private static readonly Dictionary<string, Type> TypeMap = new Dictionary<string, Type>
+                                                                 {
+                                                                    { JsonMessageKey.Heartbeat, typeof(Heartbeat) },
+                                                                    { JsonMessageKey.Info, typeof(Info) },
+                                                                    { JsonMessageKey.Options, typeof(Options) },
+                                                                    { JsonMessageKey.SimulationInfo, typeof(SimulationInfo) },
+                                                                    { JsonMessageKey.SlotInfo, typeof(SlotCollection) },
+                                                                    { JsonMessageKey.SlotOptions, typeof(SlotOptions) },
+                                                                    { JsonMessageKey.QueueInfo, typeof(UnitCollection) },
+                                                                    { JsonMessageKey.LogRestart, typeof(LogRestart) },
+                                                                    { JsonMessageKey.LogUpdate, typeof(LogUpdate) }
+                                                                 };
+
+      /// <summary>
+      /// Initializes a new instance of the FahClient class.
+      /// </summary>
+      [CoverageExclude]
+      public TypedMessageConnection()
+         : this(new TcpClientFactory())
+      {
+
+      }
+
+      /// <summary>
+      /// Initializes a new instance of the FahClient class.
+      /// </summary>
+      internal TypedMessageConnection(ITcpClientFactory tcpClientFactory)
+         : base(tcpClientFactory)
+      {
+
+      }
+
+      /// <summary>
+      /// Raise the MessageReceived event.
+      /// </summary>
+      /// <param name="e">Event arguments (if null the event is cancelled).</param>
+      protected override void OnMessageReceived(MessageReceivedEventArgs e)
+      {
+         if (e == null) return;
+
+         var dataType = TypeMap.ContainsKey(e.JsonMessage.Key) ? TypeMap[e.JsonMessage.Key] : null;
+         if (dataType != null)
+         {
+            var typedMessage = (TypedMessage)Activator.CreateInstance(dataType);
+            typedMessage.Fill(e.JsonMessage);
+            e.TypedMessage = typedMessage;
+         }
+         base.OnMessageReceived(e);
+      }
+   }
+}
