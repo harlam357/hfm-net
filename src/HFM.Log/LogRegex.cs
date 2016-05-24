2ca184263c269d37dd31d6a0884dc2964dd592eb
harlam357 <harlam357@gmail.com>
2016-05-23 22:42:27 -0500

    Regex optimization.  Remove unused captures.  Explicitly define time stamp pattern.

--- src/HFM.Log/LogRegex.cs
+++ src/HFM.Log/LogRegex.cs
@@ -5,7 +5,7 @@ namespace HFM.Log
 {
    internal static class LogRegex
    {
-      private const RegexOptions Options = RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.Singleline;
+      private const RegexOptions Options = RegexOptions.Compiled | RegexOptions.ExplicitCapture;
 
       internal static class Common
       {
@@ -13,19 +13,19 @@ namespace HFM.Log
          /// Regular Expression to match Work Unit Project string.
          /// </summary>
          internal static readonly Regex ProjectIDRegex =
-            new Regex("\\[?(?<Timestamp>.{8})[\\]|:].*Project: (?<ProjectNumber>\\d+) \\(Run (?<Run>\\d+), Clone (?<Clone>\\d+), Gen (?<Gen>\\d+)\\)", Options);
+            new Regex("\\[?\\d{2}:\\d{2}:\\d{2}[\\]|:].*Project: (?<ProjectNumber>\\d+) \\(Run (?<Run>\\d+), Clone (?<Clone>\\d+), Gen (?<Gen>\\d+)\\)", Options);
 
          /// <summary>
          /// Regular Expression to match Core Version string.
          /// </summary>
          internal static readonly Regex CoreVersionRegex =
-            new Regex("\\[?(?<Timestamp>.{8})[\\]|:].*Version (?<CoreVer>.*) \\(.*\\)", Options);
+            new Regex("\\[?\\d{2}:\\d{2}:\\d{2}[\\]|:].*Version (?<CoreVer>.*) \\(.*\\)", Options);
 
          /// <summary>
          /// Regular Expression to match Standard and SMP Clients Frame Completion Lines (Gromacs Style).
          /// </summary>
          internal static readonly Regex FramesCompletedRegex =
-            new Regex("\\[?(?<Timestamp>.{8})[\\]|:].*Completed (?<Completed>.*) out of (?<Total>.*) steps {1,2}\\((?<Percent>.*)\\)", Options);
+            new Regex("\\[?(?<Timestamp>\\d{2}:\\d{2}:\\d{2})[\\]|:].*Completed (?<Completed>.*) out of (?<Total>.*) steps {1,2}\\((?<Percent>.*)\\)", Options);
 
          /// <summary>
          /// Regular Expression to match Percent Style 1
@@ -43,13 +43,13 @@ namespace HFM.Log
          /// Regular Expression to match GPU2 Client Frame Completion Lines
          /// </summary>
          internal static readonly Regex FramesCompletedGpuRegex =
-            new Regex("\\[?(?<Timestamp>.{8})[\\]|:].*Completed (?<Percent>[0-9]{1,3})%", Options);
+            new Regex("\\[?(?<Timestamp>\\d{2}:\\d{2}:\\d{2})[\\]|:].*Completed (?<Percent>[0-9]{1,3})%", Options);
 
          /// <summary>
          /// Regular Expression to match Machine ID string.
          /// </summary>
          internal static readonly Regex CoreShutdownRegex =
-            new Regex("\\[?(?<Timestamp>.{8})[\\]|:].*Folding@home Core Shutdown: (?<UnitResult>.*)", Options);
+            new Regex("\\[?\\d{2}:\\d{2}:\\d{2}[\\]|:].*Folding@home Core Shutdown: (?<UnitResult>.*)", Options);
       }
 
       internal static class Legacy
@@ -66,37 +66,37 @@ namespace HFM.Log
          /// Regular Expression to match User (Folding ID) and Team string.
          /// </summary>
          internal static readonly Regex UserTeamRegex =
-            new Regex("\\[(?<Timestamp>.{8})\\] - User name: (?<Username>.*) \\(Team (?<TeamNumber>.*)\\)", Options);
+            new Regex("\\[\\d{2}:\\d{2}:\\d{2}\\] - User name: (?<Username>.*) \\(Team (?<TeamNumber>.*)\\)", Options);
 
          /// <summary>
          /// Regular Expression to match User ID string.
          /// </summary>
          internal static readonly Regex ReceivedUserIDRegex =
-            new Regex("\\[(?<Timestamp>.{8})\\].*- Received User ID = (?<UserID>.*)", Options);
+            new Regex("\\[\\d{2}:\\d{2}:\\d{2}\\].*- Received User ID = (?<UserID>.*)", Options);
 
          /// <summary>
          /// Regular Expression to match User ID string.
          /// </summary>
          internal static readonly Regex UserIDRegex =
-            new Regex("\\[(?<Timestamp>.{8})\\] - User ID: (?<UserID>.*)", Options);
+            new Regex("\\[\\d{2}:\\d{2}:\\d{2}\\] - User ID: (?<UserID>.*)", Options);
 
          /// <summary>
          /// Regular Expression to match Machine ID string.
          /// </summary>
          internal static readonly Regex MachineIDRegex =
-            new Regex("\\[(?<Timestamp>.{8})\\] - Machine ID: (?<MachineID>.*)", Options);
+            new Regex("\\[\\d{2}:\\d{2}:\\d{2}\\] - Machine ID: (?<MachineID>.*)", Options);
 
          /// <summary>
          /// Regular Expression to match Unit Index string.
          /// </summary>
          internal static readonly Regex UnitIndexRegex =
-            new Regex("\\[(?<Timestamp>.{8})\\] Working on Unit 0(?<QueueIndex>[\\d]) \\[.*\\]", Options);
+            new Regex("\\[\\d{2}:\\d{2}:\\d{2}\\] Working on Unit 0(?<QueueIndex>[\\d]) \\[.*\\]", Options);
 
          /// <summary>
          /// Regular Expression to match Unit Index string.
          /// </summary>
          internal static readonly Regex QueueIndexRegex =
-            new Regex("\\[(?<Timestamp>.{8})\\] Working on queue slot 0(?<QueueIndex>[\\d]) \\[.*\\]", Options);
+            new Regex("\\[\\d{2}:\\d{2}:\\d{2}\\] Working on queue slot 0(?<QueueIndex>[\\d]) \\[.*\\]", Options);
 
          /*** ProtoMol Only */
 
@@ -104,7 +104,7 @@ namespace HFM.Log
          /// Regular Expression to match ProtoMol Core Version string.
          /// </summary>
          internal static readonly Regex ProtoMolCoreVersionRegex =
-            new Regex("\\[(?<Timestamp>.{8})\\]   Version: (?<CoreVer>.*)", Options);
+            new Regex("\\[\\d{2}:\\d{2}:\\d{2}\\]   Version: (?<CoreVer>.*)", Options);
 
          /*******************/
 
@@ -112,14 +112,14 @@ namespace HFM.Log
          /// Regular Expression to match Completed Work Units string.
          /// </summary>
          internal static readonly Regex CompletedWorkUnitsRegex =
-            new Regex("\\[(?<Timestamp>.{8})\\] \\+ Number of Units Completed: (?<Completed>.*)", Options);
+            new Regex("\\[\\d{2}:\\d{2}:\\d{2}\\] \\+ Number of Units Completed: (?<Completed>.*)", Options);
 
          /// <summary>
          /// Regular Expression to match Calling Core string.  Only matches the up to the first three digits of "-np X".
          /// For this legacy detection we're only interested in knowing if this -np number exists and is greater than 1.
          /// </summary>
          internal static readonly Regex WorkUnitCallingCore =
-            new Regex("\\[(?<Timestamp>.{8})\\].*-np (?<Threads>\\d{1,3}).*", Options);
+            new Regex("\\[\\d{2}:\\d{2}:\\d{2}\\].*-np (?<Threads>\\d{1,3}).*", Options);
 
          // ReSharper restore InconsistentNaming
       }
@@ -131,22 +131,22 @@ namespace HFM.Log
 
          // a copy of this regex exists in HFM.Core.DataTypes
          internal static readonly Regex WorkUnitRunningRegex =
-            new Regex("(?<Timestamp>.{8}):WU(?<UnitIndex>\\d{2}):FS(?<FoldingSlot>\\d{2}):.*", Options);
+            new Regex("(?<Timestamp>\\d{2}:\\d{2}:\\d{2}):WU(?<UnitIndex>\\d{2}):FS(?<FoldingSlot>\\d{2}):.*", Options);
 
          internal static readonly Regex WorkUnitWorkingRegex =
-            new Regex("(?<Timestamp>.{8}):WU(?<UnitIndex>\\d{2}):FS(?<FoldingSlot>\\d{2}):Starting", Options);
+            new Regex("\\d{2}:\\d{2}:\\d{2}:WU\\d{2}:FS\\d{2}:Starting", Options);
 
          internal static readonly Regex WorkUnitCoreReturnRegex =
-            new Regex("(?<Timestamp>.{8}):WU(?<UnitIndex>\\d{2}):FS(?<FoldingSlot>\\d{2}):FahCore returned: (?<UnitResult>.*) \\(.*\\)", Options);
+            new Regex("\\d{2}:\\d{2}:\\d{2}:WU\\d{2}:FS\\d{2}:FahCore returned: (?<UnitResult>.*) \\(.*\\)", Options);
 
          internal static readonly Regex WorkUnitCleanUpRegex =
-            new Regex("(?<Timestamp>.{8}):WU(?<UnitIndex>\\d{2}):FS(?<FoldingSlot>\\d{2}):Cleaning up", Options);
+            new Regex("\\d{2}:\\d{2}:\\d{2}:WU\\d{2}:FS\\d{2}:Cleaning up", Options);
       }
    }
 
    internal static class UnitInfoRegex
    {
       internal static readonly Regex RegexProjectNumberFromTag =
-         new Regex("P(?<ProjectNumber>.*)R(?<Run>.*)C(?<Clone>.*)G(?<Gen>.*)", RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.Singleline);
+         new Regex("P(?<ProjectNumber>.*)R(?<Run>.*)C(?<Clone>.*)G(?<Gen>.*)", RegexOptions.Compiled | RegexOptions.ExplicitCapture);
    }
 }
