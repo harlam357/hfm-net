359b750e66cecfc96f295bc6676131327d5d2e63
harlam357 <harlam357@gmail.com>
2016-06-30 23:58:36 -0500

    Add HFM.Setup.CustomActions WiX project with an action to facilitate deleting files from the AppDataFolder.  UnitInfoCache.dat will be deleted on install, if present.  Hook into HFM.Setup WiX project.  Update ARPHELPLINK value and HFM.PARAFFIN file.

---
+++ src/HFM.Setup.CustomActions/CustomAction.cs
@@ -0,0 +1,23 @@
+﻿
+using System.IO;
+
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace HFM.Setup.CustomActions
+{
+   public class CustomActions
+   {
+      [CustomAction]
+      public static ActionResult DeleteAppDataFolderFilesAction(Session session)
+      {
+         string appDataFolderPath = session["AppDataFolder"];
+         string unitInfoCachePath = Path.Combine(appDataFolderPath, "HFM", "UnitInfoCache.dat");
+         if (File.Exists(unitInfoCachePath))
+         {
+            session.Log("Deleting {0}", unitInfoCachePath);
+            File.Delete(unitInfoCachePath);
+         }
+         return ActionResult.Success;
+      }
+   }
+}
