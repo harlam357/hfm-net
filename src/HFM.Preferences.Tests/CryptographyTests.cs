7d1589ce64647ad871533880ce31bf949456066f
harlam357 <harlam357@gmail.com>
2018-09-08 17:33:09 -0500

    Implement encryption locally and remove dependency on harlam357.Core.

---
+++ src/HFM.Preferences.Tests/CryptographyTests.cs
@@ -0,0 +1,42 @@
+﻿
+using NUnit.Framework;
+
+namespace HFM.Preferences
+{
+   [TestFixture]
+   public class CryptographyTests
+   {
+      [Test]
+      public void Cryptography_EncryptValue_Test()
+      {
+         // Arrange
+         string plainText = "fizzbizz";
+         // Act
+         string encryptedText = Support.Cryptography.EncryptValue(plainText);
+         // Assert
+         Assert.AreEqual("8YsqRpczouPuCPApFum1YQ==", encryptedText);
+      }
+
+      [Test]
+      public void Cryptography_DecryptValue_Test()
+      {
+         // Arrange
+         string encryptedText = "8YsqRpczouPuCPApFum1YQ==";
+         // Act
+         string plainText = Support.Cryptography.DecryptValue(encryptedText);
+         // Assert
+         Assert.AreEqual("fizzbizz", plainText);
+      }
+
+      [Test]
+      public void Cryptography_DecryptValue_ThatIsNotEncrypted_Test()
+      {
+         // Arrange
+         string encryptedText = "notencrypted";
+         // Act
+         string plainText = Support.Cryptography.DecryptValue(encryptedText);
+         // Assert
+         Assert.AreEqual("notencrypted", plainText);
+      }
+   }
+}
--- src/HFM.Preferences/Support/Cryptography.cs
+++ src/HFM.Preferences/Support/Cryptography.cs
@@ -1,17 +1,15 @@
 ﻿
 using System;
+using System.IO;
 using System.Security.Cryptography;
-
-using harlam357.Core.Security;
-using harlam357.Core.Security.Cryptography;
+using System.Text;
 
 namespace HFM.Preferences.Support
 {
    internal static class Cryptography
    {
-      // ReSharper disable once InconsistentNaming
-      private static readonly SymmetricKeyData IV = new SymmetricKeyData("3k1vKL=Cz6!wZS`I");
-      private static readonly SymmetricKeyData SymmetricKey = new SymmetricKeyData("%`Bb9ega;$.GUDaf");
+      private const string IV = "3k1vKL=Cz6!wZS`I";
+      private const string SymmetricKey = "%`Bb9ega;$.GUDaf";
 
       internal static string DecryptValue(string value)
       {
@@ -20,28 +18,45 @@ namespace HFM.Preferences.Support
             return String.Empty;
          }
 
-         using (var symmetricProvider = new Symmetric(SymmetricProvider.Rijndael, false))
+         string result;
+
+         try
          {
-            string result;
-
-            try
-            {
-               symmetricProvider.IntializationVector = IV;
-               result = symmetricProvider.Decrypt(new harlam357.Core.Security.Data(value.FromBase64()), SymmetricKey).ToString();
-            }
-            catch (FormatException)
-            {
-               // return the value as is
-               result = value;
-            }
-            catch (CryptographicException)
-            {
-               // return the value as is
-               result = value;
-            }
-
-            return result;
+            result = DecryptInternal(value);
          }
+         catch (FormatException)
+         {
+            // return the value as is
+            result = value;
+         }
+         catch (CryptographicException)
+         {
+            // return the value as is
+            result = value;
+         }
+
+         return result;
+      }
+
+      internal static string DecryptInternal(string value)
+      {
+         string plainText;
+         using (var algorithm = CreateSymmetricAlgorithm())
+         {
+            using (var ms = new MemoryStream(Convert.FromBase64String(value)))
+            {
+               using (var cs = new CryptoStream(ms, algorithm.CreateDecryptor(), CryptoStreamMode.Read))
+               {
+                  using (var sr = new StreamReader(cs))
+                  {
+                     plainText = sr.ReadToEnd();
+                  }
+               }
+            }
+
+         }
+
+         return plainText;
       }
 
       internal static string EncryptValue(string value)
@@ -51,23 +66,85 @@ namespace HFM.Preferences.Support
             return String.Empty;
          }
 
-         using (var symmetricProvider = new Symmetric(SymmetricProvider.Rijndael, false))
+         string result;
+
+         try
          {
-            string result;
-
-            try
-            {
-               symmetricProvider.IntializationVector = IV;
-               result = symmetricProvider.Encrypt(new harlam357.Core.Security.Data(value), SymmetricKey).Bytes.ToBase64();
-            }
-            catch (CryptographicException)
-            {
-               // return the value as is
-               result = value;
-            }
-
-            return result;
+            result = EncryptInternal(value);
          }
+         catch (CryptographicException)
+         {
+            // return the value as is
+            result = value;
+         }
+
+         return result;
+      }
+
+      internal static string EncryptInternal(string value)
+      {
+         byte[] encrypted;
+         using (var algorithm = CreateSymmetricAlgorithm())
+         {
+            using (var ms = new MemoryStream())
+            {
+               using (var cs = new CryptoStream(ms, algorithm.CreateEncryptor(), CryptoStreamMode.Write))
+               {
+                  using (var sw = new StreamWriter(cs))
+                  {
+                     sw.Write(value);
+                  }
+                  encrypted = ms.ToArray();
+               }
+            }
+         }
+
+         return Convert.ToBase64String(encrypted);
+      }
+
+      private static SymmetricAlgorithm CreateSymmetricAlgorithm()
+      {
+         var rijndaelManaged = new RijndaelManaged();
+         SetKey(rijndaelManaged, Encoding.UTF8.GetBytes(SymmetricKey));
+         SetIV(rijndaelManaged, Encoding.UTF8.GetBytes(IV));
+         return rijndaelManaged;
+      }
+
+      private static void SetKey(SymmetricAlgorithm algorithm, byte[] key)
+      {
+         int minBytes = algorithm.LegalKeySizes[0].MinSize / 8;
+         int maxBytes = algorithm.LegalKeySizes[0].MaxSize / 8;
+         algorithm.Key = ResizeByteArray(key, minBytes, maxBytes);
+      }
+
+      private static void SetIV(SymmetricAlgorithm algorithm, byte[] iv)
+      {
+         int minBytes = algorithm.BlockSize / 8;
+         int maxBytes = algorithm.BlockSize / 8;
+         algorithm.IV = ResizeByteArray(iv, minBytes, maxBytes);
+      }
+
+      private static byte[] ResizeByteArray(byte[] bytes, int minBytes, int  maxBytes)
+      {
+         if (maxBytes > 0)
+         {
+            if (bytes.Length > maxBytes)
+            {
+               var b = new byte[maxBytes];
+               Array.Copy(bytes, b, b.Length);
+               bytes = b;
+            }
+         }
+         if (minBytes > 0)
+         {
+            if (bytes.Length < minBytes)
+            {
+               var b = new byte[minBytes];
+               Array.Copy(bytes, b, bytes.Length);
+               bytes = b;
+            }
+         }
+         return bytes;
       }
    }
 }
