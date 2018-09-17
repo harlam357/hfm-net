ce3bdcc0d314633e5c1da280ca968eac16b0a35d
harlam357 <harlam357@gmail.com>
2018-09-16 23:36:50 -0500

    Remove comparison interfaces from UnitFrame.

--- src/HFM.Log/UnitFrame.cs
+++ src/HFM.Log/UnitFrame.cs
@@ -22,113 +22,17 @@ using System.Globalization;
 
 namespace HFM.Log
 {
-   public sealed class UnitFrame : IComparable<UnitFrame>, IEquatable<UnitFrame>
+   public class UnitFrame
    {
+      public int FrameID { get; set; }
+
       public int RawFramesComplete { get; set; }
 
       public int RawFramesTotal { get; set; }
-   
-      public int FrameID { get; set; }
 
       public TimeSpan TimeOfFrame { get; set; }
 
       public TimeSpan FrameDuration { get; set; }
-
-      ///<summary>
-      ///Serves as a hash function for a particular type. <see cref="M:System.Object.GetHashCode"></see> is suitable for use in hashing algorithms and data structures like a hash table.
-      ///</summary>
-      ///<returns>
-      ///A hash code for the current <see cref="T:System.Object"></see>.
-      ///</returns>
-      ///<filterpriority>2</filterpriority>
-      public override int GetHashCode()
-      {
-         return FrameID.GetHashCode() ^
-                TimeOfFrame.GetHashCode() ^
-                FrameDuration.GetHashCode();
-      }
-
-      ///<summary>
-      ///Determines whether the specified <see cref="T:System.Object"></see> is equal to the current <see cref="T:System.Object"></see>.
-      ///</summary>
-      ///<returns>
-      ///true if the specified <see cref="T:System.Object"></see> is equal to the current <see cref="T:System.Object"></see>; otherwise, false.
-      ///</returns>
-      ///<param name="obj">The <see cref="T:System.Object"></see> to compare with the current <see cref="T:System.Object"></see>.</param>
-      ///<filterpriority>2</filterpriority>
-      public override bool Equals(object obj)
-      {
-         var frame = obj as UnitFrame;
-         return frame != null ? Equals(frame) : base.Equals(obj);
-      }
-
-      ///<summary>
-      ///Determines whether the specified <see cref="T:HFM.Core.DataTypes.UnitFrame"></see> is equal to the current <see cref="T:HFM.Core.DataTypes.UnitFrame"></see>.
-      ///</summary>
-      ///<returns>
-      ///true if the specified <see cref="T:HFM.Core.DataTypes.UnitFrame"></see> is equal to the current <see cref="T:HFM.Core.DataTypes.UnitFrame"></see>; otherwise, false.
-      ///</returns>
-      ///<param name="other">The <see cref="T:HFM.Core.DataTypes.UnitFrame"></see> to compare with the current <see cref="T:HFM.Core.DataTypes.UnitFrame"></see>.</param>
-      public bool Equals(UnitFrame other)
-      {
-         if (other == null) return false;
-
-         return FrameID.Equals(other.FrameID) &&
-                TimeOfFrame.Equals(other.TimeOfFrame) &&
-                FrameDuration.Equals(other.FrameDuration);
-      }
-
-      public static bool operator == (UnitFrame uf1, UnitFrame uf2)
-      {
-         return ReferenceEquals(uf1, null) ? ReferenceEquals(uf2, null) : uf1.Equals(uf2);
-      }
-
-      public static bool operator != (UnitFrame uf1, UnitFrame uf2)
-      {
-         return !(uf1 == uf2);
-      }
-
-      ///<summary>
-      ///Compares the current object with another object of the same type.
-      ///</summary>
-      ///<returns>
-      ///A 32-bit signed integer that indicates the relative order of the objects being compared. The return value has the following meanings: Value Meaning Less than zero This object is less than the other parameter.Zero This object is equal to other. Greater than zero This object is greater than other. 
-      ///</returns>
-      ///<param name="other">An object to compare with this object.</param>
-      public int CompareTo(UnitFrame other)
-      {
-         if (other == null)
-         {
-            return 1;
-         }
-
-         if (FrameID.Equals(other.FrameID))
-         {
-            if (TimeOfFrame.Equals(other.TimeOfFrame))
-            {
-               if (FrameDuration.Equals(other.FrameDuration))
-               {
-                  return 0;
-               }
-
-               return FrameDuration.CompareTo(other.FrameDuration);
-            }
-
-            return TimeOfFrame.CompareTo(other.TimeOfFrame);
-         }
-
-         return FrameID.CompareTo(other.FrameID);
-      }
-
-      public static bool operator < (UnitFrame uf1, UnitFrame uf2)
-      {
-         return uf1 == null ? uf2 != null : uf1.CompareTo(uf2) < 0;
-      }
-
-      public static bool operator > (UnitFrame uf1, UnitFrame uf2)
-      {
-         return uf2 == null ? uf1 != null : uf2.CompareTo(uf1) < 0;
-      }
 
       public override string ToString()
       {
