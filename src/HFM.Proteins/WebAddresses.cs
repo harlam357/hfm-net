fcad1d3f11717899325c1691436a65185220c1e4
harlam357 <harlam357@gmail.com>
2020-02-16 13:38:47 -0600

    Add detection for OPENMM_22 Change project summary json to https

--- src/HFM.Proteins/WebAddresses.cs
+++ src/HFM.Proteins/WebAddresses.cs
@@ -10,7 +10,7 @@ namespace HFM.Proteins
       public static readonly string ProjectSummaryHtml = "https://apps.foldingathome.org/psummary.html";
       public static readonly string ProjectSummaryBetaHtml = "https://apps.foldingathome.org/psummaryB.html";
       public static readonly string ProjectSummaryCompleteHtml = "https://apps.foldingathome.org/psummaryC.html";
-      public static readonly string ProjectSummaryJson = "http://assign.stanford.edu/api/project/summary";
+      public static readonly string ProjectSummaryJson = "https://assign.stanford.edu/api/project/summary";
 #pragma warning restore 1591
    }
 }
