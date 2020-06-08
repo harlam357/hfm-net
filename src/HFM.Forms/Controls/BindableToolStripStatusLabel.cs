2341bc50f02f29952d8b5f351f09dd1371feb875
harlam357 <harlam357@gmail.com>
2020-06-07 21:30:00 -0500

    Migrate BindableToolStripStatusLabel from harlam357.Windows.Forms

---
+++ src/HFM.Forms/Controls/BindableToolStripStatusLabel.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace HFM.Forms.Controls
+{
+    [ToolStripItemDesignerAvailability(ToolStripItemDesignerAvailability.StatusStrip)]
+    public class BindableToolStripStatusLabel : ToolStripStatusLabel, IBindableComponent
+    {
+        private BindingContext _context;
+
+        public BindingContext BindingContext
+        {
+            get => _context ?? (_context = new BindingContext());
+            set => _context = value;
+        }
+
+        private ControlBindingsCollection _bindings;
+
+        public ControlBindingsCollection DataBindings
+        {
+            get => _bindings ?? (_bindings = new ControlBindingsCollection(this));
+            set => _bindings = value;
+        }
+    }
+}
