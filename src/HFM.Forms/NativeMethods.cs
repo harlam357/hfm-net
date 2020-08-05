40bf6dc951e0e10ee8c23c8104187a3cc3092f78
harlam357 <harlam357@gmail.com>
2020-08-05 11:52:40 -0500

    remove commented

--- src/HFM.Forms/NativeMethods.cs
+++ src/HFM.Forms/NativeMethods.cs
@@ -20,20 +20,8 @@ namespace HFM.Forms
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         internal static extern IntPtr SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
 
-        //[DllImport("user32.dll")]
-        //internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        //[DllImport("user32.dll", SetLastError = true)]
-        //internal static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool SetForegroundWindow(IntPtr hWnd);
-
-        //internal static void ShowToFront(IntPtr hWnd)
-        //{
-        //   ShowWindow(hWnd, 1);
-        //   SetForegroundWindow(hWnd);
-        //}
     }
 }
