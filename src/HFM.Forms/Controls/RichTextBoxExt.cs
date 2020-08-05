4a3fdb4ce9dc444812ef8a9087377c6d6a470bd2
harlam357 <harlam357@gmail.com>
2020-08-05 11:55:13 -0500

    format

--- src/HFM.Forms/Controls/RichTextBoxExt.cs
+++ src/HFM.Forms/Controls/RichTextBoxExt.cs
@@ -30,207 +30,207 @@ using HFM.Log;
 
 namespace HFM.Forms.Controls
 {
-   public interface ILogFileViewer
-   {
-      bool Visible { get; set; }
-   
-      string LogOwnedByInstanceName { get; }
-      
-      void SetLogLines(IEnumerable<LogLine> lines, string logOwnedByInstance, bool highlightLines);
+    public interface ILogFileViewer
+    {
+        bool Visible { get; set; }
 
-      void HighlightLines(bool value);
+        string LogOwnedByInstanceName { get; }
 
-      string[] Lines { get; }
-      
-      void SetNoLogLines();
-      
-      void ScrollToBottom();
-   }
+        void SetLogLines(IEnumerable<LogLine> lines, string logOwnedByInstance, bool highlightLines);
 
-   [ExcludeFromCodeCoverage]
-   public partial class RichTextBoxExt : RichTextBox, ILogFileViewer
-   {
-      private IList<LogLine> _logLines;
-      
-      private string _logOwnedByInstanceName = String.Empty;
-      
-      public string LogOwnedByInstanceName
-      {
-         get { return _logOwnedByInstanceName; }
-      }
-   
-      public RichTextBoxExt()
-      {
-         InitializeComponent();
-      }
+        void HighlightLines(bool value);
 
-      private const int MaxDisplayableLogLines = 500;
+        string[] Lines { get; }
 
-      public void SetLogLines(IEnumerable<LogLine> lines, string logOwnedByInstance, bool highlightLines)
-      {
-         if (InvokeRequired)
-         {
-            Invoke(new Action<IEnumerable<LogLine>, string, bool>(SetLogLines), lines, logOwnedByInstance, highlightLines);
-            return;
-         }
+        void SetNoLogLines();
 
-         _logOwnedByInstanceName = logOwnedByInstance;
+        void ScrollToBottom();
+    }
+
+    [ExcludeFromCodeCoverage]
+    public partial class RichTextBoxExt : RichTextBox, ILogFileViewer
+    {
+        private IList<LogLine> _logLines;
+
+        private string _logOwnedByInstanceName = String.Empty;
+
+        public string LogOwnedByInstanceName
+        {
+            get { return _logOwnedByInstanceName; }
+        }
+
+        public RichTextBoxExt()
+        {
+            InitializeComponent();
+        }
+
+        private const int MaxDisplayableLogLines = 500;
+
+        public void SetLogLines(IEnumerable<LogLine> lines, string logOwnedByInstance, bool highlightLines)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<IEnumerable<LogLine>, string, bool>(SetLogLines), lines, logOwnedByInstance, highlightLines);
+                return;
+            }
+
+            _logOwnedByInstanceName = logOwnedByInstance;
 
 #if !LOGTOOL
-         // limit the maximum number of log lines
-         int lineOffset = lines.Count() - MaxDisplayableLogLines;
-         if (lineOffset > 0)
-         {
-            lines = lines.Where((x, i) => i > lineOffset);
-         }
+            // limit the maximum number of log lines
+            int lineOffset = lines.Count() - MaxDisplayableLogLines;
+            if (lineOffset > 0)
+            {
+                lines = lines.Where((x, i) => i > lineOffset);
+            }
 #endif
 
-         _logLines = lines.ToList();
-         HighlightLines(highlightLines);
-      }
+            _logLines = lines.ToList();
+            HighlightLines(highlightLines);
+        }
 
-      public void HighlightLines(bool value)
-      {
+        public void HighlightLines(bool value)
+        {
 #if DEBUG
-         var sw = System.Diagnostics.Stopwatch.StartNew();
+            var sw = System.Diagnostics.Stopwatch.StartNew();
 #endif
-         if (value)
-         {
-            Rtf = BuildRtfString();
-         }
-         else
-         {
-            Rtf = null;
-            Lines = _logLines.Select(line => line.Raw.Replace("\r", String.Empty)).ToArray();
-         }
+            if (value)
+            {
+                Rtf = BuildRtfString();
+            }
+            else
+            {
+                Rtf = null;
+                Lines = _logLines.Select(line => line.Raw.Replace("\r", String.Empty)).ToArray();
+            }
 #if DEBUG
-         System.Diagnostics.Debug.WriteLine("HighlightLines: {0:#,##0} ms", sw.ElapsedMilliseconds);
+            System.Diagnostics.Debug.WriteLine("HighlightLines: {0:#,##0} ms", sw.ElapsedMilliseconds);
 #endif
-      }
+        }
 
-      private string BuildRtfString()
-      {
-         // cf1 - Dark Green
-         // cf2 - Dark Red
-         // cf3 - Dark Orange
-         // cf4 - Blue
-         // cf5 - Slate Gray
+        private string BuildRtfString()
+        {
+            // cf1 - Dark Green
+            // cf2 - Dark Red
+            // cf3 - Dark Orange
+            // cf4 - Blue
+            // cf5 - Slate Gray
 
-         var sb = new StringBuilder();
-         sb.Append(@"{\rtf1\ansi\deff0{\colortbl;\red0\green150\blue0;\red139\green0\blue0;\red255\green140\blue0;\red0\green0\blue255;\red120\green120\blue120;}");
-         foreach(var line in _logLines)
-         {
-            sb.AppendFormat(CultureInfo.InvariantCulture, @"{0}{1}\line", GetLineColor(line), line);
-         }
-         return sb.ToString();
-      }
+            var sb = new StringBuilder();
+            sb.Append(@"{\rtf1\ansi\deff0{\colortbl;\red0\green150\blue0;\red139\green0\blue0;\red255\green140\blue0;\red0\green0\blue255;\red120\green120\blue120;}");
+            foreach (var line in _logLines)
+            {
+                sb.AppendFormat(CultureInfo.InvariantCulture, @"{0}{1}\line", GetLineColor(line), line);
+            }
+            return sb.ToString();
+        }
 
-      private static string GetLineColor(LogLine line)
-      {
-         if (line.LineType == LogLineType.None)
-         {
-            return @"\cf5 ";
-         }
+        private static string GetLineColor(LogLine line)
+        {
+            if (line.LineType == LogLineType.None)
+            {
+                return @"\cf5 ";
+            }
 
-         if (line.Data is LogLineDataParserError)
-         {
-            return @"\cf3 ";
-         }
+            if (line.Data is LogLineDataParserError)
+            {
+                return @"\cf3 ";
+            }
 
-         if (line.LineType == LogLineType.WorkUnitFrame)
-         {
-            return @"\cf1 ";
-         }
+            if (line.LineType == LogLineType.WorkUnitFrame)
+            {
+                return @"\cf1 ";
+            }
 
-         if (line.LineType == LogLineType.ClientShutdown || 
-             line.LineType == LogLineType.ClientCoreCommunicationsError ||
-             line.LineType == LogLineType.ClientCoreCommunicationsErrorShutdown || 
-             line.LineType == LogLineType.ClientEuePauseState ||
-             line.LineType == LogLineType.WorkUnitCoreShutdown || 
-             line.LineType == LogLineType.WorkUnitCoreReturn)
-         {
-            return @"\cf2 ";
-         }
+            if (line.LineType == LogLineType.ClientShutdown ||
+                line.LineType == LogLineType.ClientCoreCommunicationsError ||
+                line.LineType == LogLineType.ClientCoreCommunicationsErrorShutdown ||
+                line.LineType == LogLineType.ClientEuePauseState ||
+                line.LineType == LogLineType.WorkUnitCoreShutdown ||
+                line.LineType == LogLineType.WorkUnitCoreReturn)
+            {
+                return @"\cf2 ";
+            }
 
-         return @"\cf4 ";
-      }
-      
-      public void SetNoLogLines()
-      {
-         if (InvokeRequired)
-         {
-            Invoke(new MethodInvoker(SetNoLogLines));
-            return;
-         }
+            return @"\cf4 ";
+        }
 
-         _logLines = null;
+        public void SetNoLogLines()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new MethodInvoker(SetNoLogLines));
+                return;
+            }
 
-         Rtf = Core.Application.IsRunningOnMono ? String.Empty : null;
-         Text = "No Log Available";
-      }
+            _logLines = null;
 
-      #region Native Scroll Messages (don't call under Mono)
+            Rtf = Core.Application.IsRunningOnMono ? String.Empty : null;
+            Text = "No Log Available";
+        }
 
-      public void ScrollToBottom()
-      {
-         if (InvokeRequired)
-         {
-            Invoke(new MethodInvoker(ScrollToBottom));
-            return;
-         }
+        #region Native Scroll Messages (don't call under Mono)
 
-         SelectionStart = TextLength;
+        public void ScrollToBottom()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new MethodInvoker(ScrollToBottom));
+                return;
+            }
 
-         if (Core.Application.IsRunningOnMono)
-         {
-            ScrollToCaret();
-         }
-         else
-         {
-            Internal.NativeMethods.SendMessage(Handle, Internal.NativeMethods.WM_VSCROLL, new IntPtr(Internal.NativeMethods.SB_BOTTOM), new IntPtr(0));
-         }
-      }
+            SelectionStart = TextLength;
 
-      public void ScrollToTop()
-      {
-         if (Core.Application.IsRunningOnMono)
-         {
-            throw new NotImplementedException("This function is not implemented when running under the Mono Runtime.");
-         }
+            if (Core.Application.IsRunningOnMono)
+            {
+                ScrollToCaret();
+            }
+            else
+            {
+                Internal.NativeMethods.SendMessage(Handle, Internal.NativeMethods.WM_VSCROLL, new IntPtr(Internal.NativeMethods.SB_BOTTOM), new IntPtr(0));
+            }
+        }
 
-         Internal.NativeMethods.SendMessage(Handle, Internal.NativeMethods.WM_VSCROLL, new IntPtr(Internal.NativeMethods.SB_TOP), new IntPtr(0));
-      }
+        public void ScrollToTop()
+        {
+            if (Core.Application.IsRunningOnMono)
+            {
+                throw new NotImplementedException("This function is not implemented when running under the Mono Runtime.");
+            }
 
-      public void ScrollLineDown()
-      {
-         if (Core.Application.IsRunningOnMono)
-         {
-            throw new NotImplementedException("This function is not implemented when running under the Mono Runtime.");
-         }
+            Internal.NativeMethods.SendMessage(Handle, Internal.NativeMethods.WM_VSCROLL, new IntPtr(Internal.NativeMethods.SB_TOP), new IntPtr(0));
+        }
 
-         Internal.NativeMethods.SendMessage(Handle, Internal.NativeMethods.WM_VSCROLL, new IntPtr(Internal.NativeMethods.SB_LINEDOWN), new IntPtr(0));
-      }
+        public void ScrollLineDown()
+        {
+            if (Core.Application.IsRunningOnMono)
+            {
+                throw new NotImplementedException("This function is not implemented when running under the Mono Runtime.");
+            }
 
-      public void ScrollLineUp()
-      {
-         if (Core.Application.IsRunningOnMono)
-         {
-            throw new NotImplementedException("This function is not implemented when running under the Mono Runtime.");
-         }
+            Internal.NativeMethods.SendMessage(Handle, Internal.NativeMethods.WM_VSCROLL, new IntPtr(Internal.NativeMethods.SB_LINEDOWN), new IntPtr(0));
+        }
 
-         Internal.NativeMethods.SendMessage(Handle, Internal.NativeMethods.WM_VSCROLL, new IntPtr(Internal.NativeMethods.SB_LINEUP), new IntPtr(0));
-      }
+        public void ScrollLineUp()
+        {
+            if (Core.Application.IsRunningOnMono)
+            {
+                throw new NotImplementedException("This function is not implemented when running under the Mono Runtime.");
+            }
 
-      public void ScrollToLine(int lineNumber)
-      {
-         if (Core.Application.IsRunningOnMono)
-         {
-            throw new NotImplementedException("This function is not implemented when running under the Mono Runtime.");
-         }
+            Internal.NativeMethods.SendMessage(Handle, Internal.NativeMethods.WM_VSCROLL, new IntPtr(Internal.NativeMethods.SB_LINEUP), new IntPtr(0));
+        }
 
-         Internal.NativeMethods.SendMessage(Handle, Internal.NativeMethods.EM_LINESCROLL, new IntPtr(0), new IntPtr(lineNumber));
-      }
-      
-      #endregion
-   }
+        public void ScrollToLine(int lineNumber)
+        {
+            if (Core.Application.IsRunningOnMono)
+            {
+                throw new NotImplementedException("This function is not implemented when running under the Mono Runtime.");
+            }
+
+            Internal.NativeMethods.SendMessage(Handle, Internal.NativeMethods.EM_LINESCROLL, new IntPtr(0), new IntPtr(lineNumber));
+        }
+
+        #endregion
+    }
 }
