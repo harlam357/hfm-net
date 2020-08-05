e38423367f10d108a093ef8387323e9d4a0e7477
harlam357 <harlam357@gmail.com>
2020-08-05 13:18:40 -0500

    Remove commented code

--- src/HFM.Forms/Extensions.cs
+++ src/HFM.Forms/Extensions.cs
@@ -6,37 +6,27 @@ namespace HFM.Forms
 {
     internal static class Extensions
     {
-        public static string ToDirectionString(this ListSortDirection direction)
+        internal static string ToDirectionString(this ListSortDirection direction)
         {
             return direction.Equals(ListSortDirection.Descending) ? "DESC" : "ASC";
         }
 
-        public static void BindText(this Control control, object dataSource, string dataMember)
+        internal static void BindText(this Control control, object dataSource, string dataMember)
         {
             control.DataBindings.Add("Text", dataSource, dataMember, false, DataSourceUpdateMode.OnValidation);
         }
 
-        //public static void BindText(this Control control, object dataSource, string dataMember, DataSourceUpdateMode updateMode)
-        //{
-        //   control.DataBindings.Add("Text", dataSource, dataMember, false, updateMode);
-        //}
-
-        public static void BindEnabled(this Control control, object dataSource, string dataMember)
+        internal static void BindEnabled(this Control control, object dataSource, string dataMember)
         {
             control.DataBindings.Add("Enabled", dataSource, dataMember, false, DataSourceUpdateMode.OnPropertyChanged);
         }
 
-        //public static void BindEnabled(this Control control, object dataSource, string dataMember, DataSourceUpdateMode updateMode)
-        //{
-        //   control.DataBindings.Add("Enabled", dataSource, dataMember, false, updateMode);
-        //}
-
-        public static void BindChecked(this CheckBox control, object dataSource, string dataMember)
+        internal static void BindChecked(this CheckBox control, object dataSource, string dataMember)
         {
             control.DataBindings.Add("Checked", dataSource, dataMember, false, DataSourceUpdateMode.OnPropertyChanged);
         }
 
-        public static void BindChecked(this RadioButton control, object dataSource, string dataMember)
+        internal static void BindChecked(this RadioButton control, object dataSource, string dataMember)
         {
             control.DataBindings.Add("Checked", dataSource, dataMember, false, DataSourceUpdateMode.OnPropertyChanged);
         }
