a35f701ccd7780d39088e45cb8e38df27d88dd04
harlam357 <harlam357@gmail.com>
2020-08-09 13:10:50 -0500

    format

--- src/HFM.Preferences/Internal/ObjectExtensions.cs
+++ src/HFM.Preferences/Internal/ObjectExtensions.cs
@@ -3,24 +3,24 @@ using System;
 
 namespace HFM.Preferences.Internal
 {
-   internal static class ObjectExtensions
-   {
-      internal static T Copy<T>(this T value)
-      {
-         if (null == (object)value)
-         {
-            return value;
-         }
-         return Copy(value, value.GetType());
-      }
+    internal static class ObjectExtensions
+    {
+        internal static T Copy<T>(this T value)
+        {
+            if (null == (object)value)
+            {
+                return value;
+            }
+            return Copy(value, value.GetType());
+        }
 
-      internal static T Copy<T>(this T value, Type dataType)
-      {
-         if (dataType.IsValueType || dataType == typeof(string) || null == (object)value)
-         {
-            return value;
-         }
-         return (T)Activator.CreateInstance(dataType, value);
-      }
-   }
+        internal static T Copy<T>(this T value, Type dataType)
+        {
+            if (dataType.IsValueType || dataType == typeof(string) || null == (object)value)
+            {
+                return value;
+            }
+            return (T)Activator.CreateInstance(dataType, value);
+        }
+    }
 }
