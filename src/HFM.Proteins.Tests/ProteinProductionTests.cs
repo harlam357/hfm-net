c6971f06b62fa8c41ef8960e26cb501881865177
harlam357 <harlam357@gmail.com>
2020-08-30 02:12:23 -0500

    Add Protein.Copy() and ProteinProduction IEquatable<ProteinProduction> tests

---
+++ src/HFM.Proteins.Tests/ProteinProductionTests.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+namespace HFM.Proteins
+{
+    [TestFixture]
+    public class ProteinProductionTests
+    {
+        [Test]
+        public void ProteinProduction_EqualsProteinProduction_ReturnsTrue()
+        {
+            // Arrange
+            var production = new ProteinProduction(1.0, 2.0, 3.0, 4.0);
+            var other = production;
+            // Act
+            var result = production.Equals(other);
+            // Assert
+            Assert.IsTrue(result);
+            Assert.AreEqual(production.GetHashCode(), other.GetHashCode());
+            Assert.IsTrue(production == other);
+            Assert.IsFalse(production != other);
+        }
+
+        [Test]
+        public void ProteinProduction_EqualsProteinProduction_ReturnsFalse()
+        {
+            // Arrange
+            var production = new ProteinProduction(1.0, 2.0, 3.0, 4.0);
+            var other = new ProteinProduction(2.0, 3.0, 4.0, 5.0);
+            // Act
+            var result = production.Equals(other);
+            // Assert
+            Assert.IsFalse(result);
+            Assert.AreNotEqual(production.GetHashCode(), other.GetHashCode());
+            Assert.IsFalse(production == other);
+            Assert.IsTrue(production != other);
+        }
+
+        [Test]
+        public void ProteinProduction_EqualsObject_ReturnsTrue()
+        {
+            // Arrange
+            var production = new ProteinProduction(1.0, 2.0, 3.0, 4.0);
+            object other = production;
+            // Act
+            var result = production.Equals(other);
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void ProteinProduction_EqualsObject_ReturnsFalse()
+        {
+            // Arrange
+            var production = new ProteinProduction(1.0, 2.0, 3.0, 4.0);
+            object other = new ProteinProduction(2.0, 3.0, 4.0, 5.0);
+            // Act
+            var result = production.Equals(other);
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
--- src/HFM.Proteins.Tests/ProteinTests.cs
+++ src/HFM.Proteins.Tests/ProteinTests.cs
@@ -24,6 +24,43 @@ namespace HFM.Proteins
         }
 
         [Test]
+        public void Protein_Copy_ReturnsNewInstance()
+        {
+            // Arrange
+            var protein = new Protein
+            {
+                ProjectNumber = 1,
+                ServerIP = nameof(Protein.ServerIP),
+                WorkUnitName = nameof(Protein.WorkUnitName),
+                NumberOfAtoms = 2,
+                PreferredDays = 3,
+                MaximumDays = 4,
+                Credit = 5,
+                Frames = 6,
+                Core = nameof(Protein.Core),
+                Description = nameof(Protein.Description),
+                Contact = nameof(Protein.Contact),
+                KFactor = 7
+            };
+            // Act
+            var copy = protein.Copy();
+            // Assert
+            Assert.AreNotSame(protein, copy);
+            Assert.AreEqual(1, copy.ProjectNumber);
+            Assert.AreEqual(nameof(Protein.ServerIP), copy.ServerIP);
+            Assert.AreEqual(nameof(Protein.WorkUnitName), copy.WorkUnitName);
+            Assert.AreEqual(2, copy.NumberOfAtoms);
+            Assert.AreEqual(3, copy.PreferredDays);
+            Assert.AreEqual(4, copy.MaximumDays);
+            Assert.AreEqual(5, copy.Credit);
+            Assert.AreEqual(6, copy.Frames);
+            Assert.AreEqual(nameof(Protein.Core), copy.Core);
+            Assert.AreEqual(nameof(Protein.Description), copy.Description);
+            Assert.AreEqual(nameof(Protein.Contact), copy.Contact);
+            Assert.AreEqual(7, copy.KFactor);
+        }
+
+        [Test]
         public void Protein_IsValid_ReturnsTrueWhenAllRequiredPropertiesArePopulated_Test()
         {
             var protein = new Protein { ProjectNumber = 1, PreferredDays = 3, MaximumDays = 5, Credit = 500, Frames = 100, KFactor = 26.4 };
