8a3e07662e08c12127aa918f20e508259ec24434
harlam357 <harlam357@gmail.com>
2020-08-29 23:44:11 -0500

    ProteinProduction.cs

---
+++ src/HFM.Proteins/ProteinProduction.cs
@@ -0,0 +1,28 @@
+﻿namespace HFM.Proteins
+{
+    /// <summary>
+    /// Represents all protein production measurements.
+    /// </summary>
+    public struct ProteinProduction
+    {
+        /// <summary>
+        /// Gets or sets the units per day (UPD) measurement.
+        /// </summary>
+        public double UPD { get; set; }
+
+        /// <summary>
+        /// Gets or sets the bonus multiplier measurement.
+        /// </summary>
+        public double Multiplier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the work unit credit measurement.
+        /// </summary>
+        public double Credit { get; set; }
+
+        /// <summary>
+        /// Gets or sets the points per day (PPD) measurement.
+        /// </summary>
+        public double PPD { get; set; }
+    }
+}
