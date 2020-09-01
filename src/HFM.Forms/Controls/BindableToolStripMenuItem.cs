4e6c874afa1cba8e7be241771959c4806fa43461
harlam357 <harlam357@gmail.com>
2020-09-01 14:55:37 -0500

    BindableToolStripMenuItem

---
+++ src/HFM.Forms/Controls/BindableToolStripMenuItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace HFM.Forms.Controls
+{
+    [ToolStripItemDesignerAvailability(ToolStripItemDesignerAvailability.StatusStrip)]
+    public class BindableToolStripMenuItem : ToolStripMenuItem, IBindableComponent
+    {
+        private BindingContext _context;
+
+        public BindingContext BindingContext
+        {
+            get => _context ?? (_context = new BindingContext());
+            set => _context = value;
+        }
+
+        private ControlBindingsCollection _bindings;
+
+        public ControlBindingsCollection DataBindings
+        {
+            get => _bindings ?? (_bindings = new ControlBindingsCollection(this));
+            set => _bindings = value;
+        }
+    }
+}
