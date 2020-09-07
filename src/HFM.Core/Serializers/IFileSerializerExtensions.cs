8de33786fa2263362eca9273766dae2ad7f285e2
harlam357 <harlam357@gmail.com>
2020-09-07 16:27:46 -0500

    format

--- src/HFM.Core/Serializers/IFileSerializerExtensions.cs
+++ src/HFM.Core/Serializers/IFileSerializerExtensions.cs
@@ -1,22 +1,21 @@
-﻿
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 
 namespace HFM.Core.Serializers
 {
-   public static class IFileSerializerExtensions
-   {
-      public static string GetFileTypeFilters<T>(this IEnumerable<IFileSerializer<T>> serializers) where T : class, new()
-      {
-         var sb = new StringBuilder();
-         foreach (var s in serializers)
-         {
-            sb.Append(s.FileTypeFilter);
-            sb.Append("|");
-         }
+    public static class IFileSerializerExtensions
+    {
+        public static string GetFileTypeFilters<T>(this IEnumerable<IFileSerializer<T>> serializers) where T : class, new()
+        {
+            var sb = new StringBuilder();
+            foreach (var s in serializers)
+            {
+                sb.Append(s.FileTypeFilter);
+                sb.Append("|");
+            }
 
-         sb.Length = sb.Length - 1;
-         return sb.ToString();
-      }
-   }
+            sb.Length = sb.Length - 1;
+            return sb.ToString();
+        }
+    }
 }
