d383c8db76e3c81f9cd5acd2d88fc10ed23ad5a4
harlam357 <harlam357@gmail.com>
2020-09-07 22:02:28 -0500

    CA1054

--- src/HFM.Core/Net/HttpUrl.cs
+++ src/HFM.Core/Net/HttpUrl.cs
@@ -5,6 +5,7 @@ namespace HFM.Core.Net
 {
     public static class HttpUrl
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1054:URI-like parameters should not be strings", Justification = "Validating string input can be converted to Uri.")]
         public static bool Validate(string url)
         {
             return Uri.TryCreate(url, UriKind.Absolute, out var result) && (result.Scheme == Uri.UriSchemeHttp || result.Scheme == Uri.UriSchemeHttps);
