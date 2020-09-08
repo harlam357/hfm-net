5c76df427cbb395b857fbff76752e93415c5bc2a
harlam357 <harlam357@gmail.com>
2020-09-08 18:13:53 -0500

    Add ICollection<T>.Reset() extension: Removes all existing items from the <see cref="ICollection{T}" /> and adds the <paramref name="items" />.

---
+++ src/HFM.Forms/Internal/CollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace HFM.Forms.Internal
+{
+    internal static class CollectionExtensions
+    {
+        /// <summary>
+        /// Removes all existing items from the <see cref="ICollection{T}" /> and adds the <paramref name="items" />.
+        /// </summary>
+        internal static void Reset<T>(this ICollection<T> source, IEnumerable<T> items)
+        {
+            source.Clear();
+            if (items != null)
+            {
+                // optimize for source List<T>
+                if (source is List<T> list)
+                {
+                    list.AddRange(items);
+                }
+                else
+                {
+                    foreach (var item in items)
+                    {
+                        source.Add(item);
+                    }
+                }
+            }
+        }
+    }
+}
