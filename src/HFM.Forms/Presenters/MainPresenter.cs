23a291ce1ce194803db9008b5b38f855b263965f
harlam357 <harlam357@gmail.com>
2020-09-15 21:19:35 -0500

    remove comment

--- src/HFM.Forms/Presenters/MainPresenter.cs
+++ src/HFM.Forms/Presenters/MainPresenter.cs
@@ -1,9 +1,4 @@
-﻿/*
- * View and DataGridView save state code based on code by Ron Dunant.
- * http://www.codeproject.com/KB/grid/PersistentDataGridView.aspx
- */
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
