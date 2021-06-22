fe7f59dc50a2ebae8a7fd4d9bc775c1f17b01bd0
firedfly <firedfly@gmail.com>
2021-06-21 22:28:28 -0400

    Add processor to the slot summary web generation output (xml and html)

--- src/HFM.Core/SlotXml/SlotData.cs
+++ src/HFM.Core/SlotXml/SlotData.cs
@@ -28,69 +28,72 @@ namespace HFM.Core.SlotXml
         public string SlotType { get; set; }
 
         [DataMember(Order = 7)]
-        public string ClientVersion { get; set; }
+        public string Processor { get; set; }
 
         [DataMember(Order = 8)]
-        public string TPF { get; set; }
+        public string ClientVersion { get; set; }
 
         [DataMember(Order = 9)]
-        public double PPD { get; set; }
+        public string TPF { get; set; }
 
         [DataMember(Order = 10)]
-        public double UPD { get; set; }
+        public double PPD { get; set; }
 
         [DataMember(Order = 11)]
-        public string ETA { get; set; }
+        public double UPD { get; set; }
 
         [DataMember(Order = 12)]
-        public string Core { get; set; }
+        public string ETA { get; set; }
 
         [DataMember(Order = 13)]
-        public string CoreId { get; set; }
+        public string Core { get; set; }
 
         [DataMember(Order = 14)]
-        public bool ProjectIsDuplicate { get; set; }
+        public string CoreId { get; set; }
 
         [DataMember(Order = 15)]
-        public string ProjectRunCloneGen { get; set; }
+        public bool ProjectIsDuplicate { get; set; }
 
         [DataMember(Order = 16)]
-        public double Credit { get; set; }
+        public string ProjectRunCloneGen { get; set; }
 
         [DataMember(Order = 17)]
-        public int Completed { get; set; }
+        public double Credit { get; set; }
 
         [DataMember(Order = 18)]
-        public int Failed { get; set; }
+        public int Completed { get; set; }
 
         [DataMember(Order = 19)]
-        public int TotalRunCompletedUnits { get; set; }
+        public int Failed { get; set; }
 
         [DataMember(Order = 20)]
-        public int TotalCompletedUnits { get; set; }
+        public int TotalRunCompletedUnits { get; set; }
 
         [DataMember(Order = 21)]
-        public int TotalRunFailedUnits { get; set; }
+        public int TotalCompletedUnits { get; set; }
 
         [DataMember(Order = 22)]
-        public int TotalFailedUnits { get; set; }
+        public int TotalRunFailedUnits { get; set; }
 
         [DataMember(Order = 23)]
-        public bool UsernameOk { get; set; }
+        public int TotalFailedUnits { get; set; }
 
         [DataMember(Order = 24)]
-        public string Username { get; set; }
+        public bool UsernameOk { get; set; }
 
         [DataMember(Order = 25)]
-        public string DownloadTime { get; set; }
+        public string Username { get; set; }
 
         [DataMember(Order = 26)]
-        public string PreferredDeadline { get; set; }
+        public string DownloadTime { get; set; }
 
         [DataMember(Order = 27)]
-        public IList<LogLine> CurrentLogLines { get; set; }
+        public string PreferredDeadline { get; set; }
 
         [DataMember(Order = 28)]
+        public IList<LogLine> CurrentLogLines { get; set; }
+
+        [DataMember(Order = 29)]
         public Protein Protein { get; set; }
     }
 }
