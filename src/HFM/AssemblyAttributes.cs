c6fcbbd79b0d41f78e462385737ef0787c1b5a92
harlam357 <harlam357@gmail.com>
2021-07-03 13:01:50 -0500

    CLSCompliant(false)

--- src/HFM/AssemblyAttributes.cs
+++ src/HFM/AssemblyAttributes.cs
@@ -1,6 +1,8 @@
-﻿using System.Resources;
+﻿using System;
+using System.Resources;
 using System.Runtime.Versioning;
 
+[assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 #if NET5_0
 [assembly: SupportedOSPlatform("windows7.0")]
