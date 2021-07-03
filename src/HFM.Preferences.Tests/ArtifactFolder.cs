836cc20e460d86ab3059c77b8abf44ae4d4ab643
harlam357 <harlam357@gmail.com>
2021-07-03 13:07:21 -0500

    GC.SuppressFinalize(this);

--- src/HFM.Preferences.Tests/ArtifactFolder.cs
+++ src/HFM.Preferences.Tests/ArtifactFolder.cs
@@ -4,32 +4,38 @@ using System.IO;
 
 namespace HFM.Preferences
 {
-   public class ArtifactFolder : IDisposable
-   {
-      public string Path { get; private set; }
+    public class ArtifactFolder : IDisposable
+    {
+        public string Path { get; private set; }
 
-      public ArtifactFolder()
-         : this(Environment.CurrentDirectory)
-      {
-         
-      }
+        public ArtifactFolder()
+           : this(Environment.CurrentDirectory)
+        {
 
-      public ArtifactFolder(string basePath)
-      {
-         Path = System.IO.Path.Combine(basePath, System.IO.Path.GetRandomFileName());
-         Directory.CreateDirectory(Path);
-      }
+        }
 
-      public void Dispose()
-      {
-         try
-         {
-            Directory.Delete(Path, true);
-         }
-         catch (Exception)
-         {
+        public ArtifactFolder(string basePath)
+        {
+            Path = System.IO.Path.Combine(basePath, System.IO.Path.GetRandomFileName());
+            Directory.CreateDirectory(Path);
+        }
 
-         }
-      }
-   }
+        public void Dispose()
+        {
+            TryDeleteDirectory();
+            GC.SuppressFinalize(this);
+        }
+
+        private void TryDeleteDirectory()
+        {
+            try
+            {
+                Directory.Delete(Path, true);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
 }
