c187ea09fb90f95af9125cdbd7b9910214d016ed
harlam357 <harlam357@gmail.com>
2022-02-05 15:07:17 -0600

    remove commented code

--- src/HFM.Core/Data/WorkUnitContext.cs
+++ src/HFM.Core/Data/WorkUnitContext.cs
@@ -1,61 +1,59 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
-namespace HFM.Core.Data
+namespace HFM.Core.Data;
+
+public partial class WorkUnitContext : DbContext
 {
-    public partial class WorkUnitContext : DbContext
+    public DbSet<WorkUnitEntity> WorkUnits { get; set; }
+    public DbSet<ProteinEntity> Proteins { get; set; }
+    public DbSet<ClientEntity> Clients { get; set; }
+    public DbSet<WorkUnitFrameEntity> WorkUnitFrames { get; set; }
+    public DbSet<VersionEntity> Versions { get; set; }
+
+    private readonly string _connectionString;
+    private readonly Action<string> _logTo;
+
+    public WorkUnitContext(string connectionString) : this(connectionString, null)
     {
-        public DbSet<WorkUnitEntity> WorkUnits { get; set; }
-        public DbSet<ProteinEntity> Proteins { get; set; }
-        public DbSet<ClientEntity> Clients { get; set; }
-        public DbSet<WorkUnitFrameEntity> WorkUnitFrames { get; set; }
-        public DbSet<VersionEntity> Versions { get; set; }
 
-        private readonly string _connectionString;
-        private readonly Action<string> _logTo;
+    }
 
-        public WorkUnitContext(string connectionString) : this(connectionString, null)
+    public WorkUnitContext(string connectionString, Action<string> logTo)
+    {
+        _connectionString = connectionString;
+        _logTo = logTo;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(_connectionString);
+        if (_logTo is not null)
         {
-
+            optionsBuilder.LogTo(_logTo, LogLevel.Information);
         }
 
-        public WorkUnitContext(string connectionString, Action<string> logTo)
-        {
-            _connectionString = connectionString;
-            _logTo = logTo;
-        }
+        base.OnConfiguring(optionsBuilder);
+    }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            //optionsBuilder.UseSqlite("Data Source=WorkUnits.db");
-            optionsBuilder.UseSqlite(_connectionString);
-            if (_logTo is not null)
-            {
-                optionsBuilder.LogTo(_logTo, LogLevel.Information);
-            }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<WorkUnitEntity>()
+            .HasOne(x => x.Protein)
+            .WithMany()
+            .HasForeignKey(x => x.ProteinID);
 
-            base.OnConfiguring(optionsBuilder);
-        }
+        modelBuilder.Entity<WorkUnitEntity>()
+            .HasOne(x => x.Client)
+            .WithMany()
+            .HasForeignKey(x => x.ClientID);
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<WorkUnitEntity>()
-                .HasOne(x => x.Protein)
-                .WithMany()
-                .HasForeignKey(x => x.ProteinID);
+        modelBuilder.Entity<WorkUnitEntity>()
+            .HasMany(x => x.Frames)
+            .WithOne()
+            .HasForeignKey(x => x.WorkUnitID);
 
-            modelBuilder.Entity<WorkUnitEntity>()
-                .HasOne(x => x.Client)
-                .WithMany()
-                .HasForeignKey(x => x.ClientID);
-
-            modelBuilder.Entity<WorkUnitEntity>()
-                .HasMany(x => x.Frames)
-                .WithOne()
-                .HasForeignKey(x => x.WorkUnitID);
-
-            modelBuilder.Entity<WorkUnitFrameEntity>()
-                .HasKey(x => new { x.WorkUnitID, x.FrameID });
-        }
+        modelBuilder.Entity<WorkUnitFrameEntity>()
+            .HasKey(x => new { x.WorkUnitID, x.FrameID });
     }
 }
