be70c1ee82c19a731cf6d61d318a5c4c2a5c368a
harlam357 <harlam357@gmail.com>
2022-03-19 16:35:02 -0500

    Add additional assemblies for EF Core Sqlite

--- src/HFM.Build.ps1
+++ src/HFM.Build.ps1
@@ -129,8 +129,6 @@ Function Deploy-Build
     # Primary Assemblies
     [string[]]$Assemblies = @(
         "AutoMapper.dll",
-        "EntityFramework.dll",
-        "EntityFramework.SqlServer.dll",
         "HFM.Client.dll",
         "HFM.Core.dll",
         "HFM.deps.json", 
@@ -144,10 +142,27 @@ Function Deploy-Build
         "HFM.runtimeconfig.json",
         "LightInject.dll",
         "LightInject.Microsoft.DependencyInjection.dll",
+        "Microsoft.Data.Sqlite.dll",
+        "Microsoft.EntityFrameworkCore.Abstractions.dll",
+        "Microsoft.EntityFrameworkCore.dll",
+        "Microsoft.EntityFrameworkCore.Relational.dll",
+        "Microsoft.EntityFrameworkCore.Sqlite.dll",
+        "Microsoft.Extensions.Caching.Abstractions.dll",
+        "Microsoft.Extensions.Caching.Memory.dll",
+        "Microsoft.Extensions.Configuration.Abstractions.dll",
         "Microsoft.Extensions.DependencyInjection.Abstractions.dll",
+        "Microsoft.Extensions.DependencyInjection.dll",
+        "Microsoft.Extensions.DependencyModel.dll",
+        "Microsoft.Extensions.Logging.Abstractions.dll",
+        "Microsoft.Extensions.Logging.dll",
+        "Microsoft.Extensions.Options.dll",
+        "Microsoft.Extensions.Primitives.dll",
         "Newtonsoft.Json.dll",
         "protobuf-net.Core.dll",
         "protobuf-net.dll",
+        "SQLitePCLRaw.batteries_v2.dll",
+        "SQLitePCLRaw.core.dll",
+        "SQLitePCLRaw.provider.e_sqlite3.dll",
         "ZedGraph.dll"
         )
 
