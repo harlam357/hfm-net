4240017d36bc72cff3682e1a99462cfa06142a56
harlam357 <harlam357@gmail.com>
2022-03-27 00:44:56 -0500

    Use System.Net.Http.Json

--- src/HFM.Core/Services/FahUserService.cs
+++ src/HFM.Core/Services/FahUserService.cs
@@ -1,45 +1,40 @@
-﻿using System;
-using System.Globalization;
-using System.Net.Http;
-using System.Threading.Tasks;
-
-using HFM.Core.Internal;
+﻿using System.Globalization;
+using System.Net.Http.Json;
 
 using Newtonsoft.Json;
 
-namespace HFM.Core.Services
+namespace HFM.Core.Services;
+
+public class FahUser
 {
-    public class FahUser
+    [JsonProperty("id")]
+    public int ID { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+}
+
+public abstract class FahUserService
+{
+    public static FahUserService Default { get; } = new DefaultFahUserService();
+
+    public abstract Task<FahUser> FindUser(string name);
+}
+
+public class DefaultFahUserService : FahUserService
+{
+    private static readonly HttpClient _Client = new();
+
+    public override async Task<FahUser> FindUser(string name)
     {
-        [JsonProperty("id")]
-        public int ID { get; set; }
-
-        [JsonProperty("name")]
-        public string Name { get; set; }
+        string uri = String.Format(CultureInfo.InvariantCulture, FahUrl.UserFindApiUrlTemplate, name);
+        return await _Client.GetFromJsonAsync<FahUser>(uri).ConfigureAwait(false);
     }
+}
 
-    public abstract class FahUserService
-    {
-        public static FahUserService Default { get; } = new DefaultFahUserService();
+public class NullFahUserService : FahUserService
+{
+    public static NullFahUserService Instance { get; } = new();
 
-        public abstract Task<FahUser> FindUser(string name);
-    }
-
-    public class DefaultFahUserService : FahUserService
-    {
-        private static readonly HttpClient _Client = new HttpClient();
-
-        public override async Task<FahUser> FindUser(string name)
-        {
-            string uri = String.Format(CultureInfo.InvariantCulture, FahUrl.UserFindApiUrlTemplate, name);
-            return await _Client.GetFromJsonAsync<FahUser>(uri).ConfigureAwait(false);
-        }
-    }
-
-    public class NullFahUserService : FahUserService
-    {
-        public static NullFahUserService Instance { get; } = new NullFahUserService();
-
-        public override Task<FahUser> FindUser(string name) => Task.FromResult(new FahUser());
-    }
+    public override Task<FahUser> FindUser(string name) => Task.FromResult(new FahUser());
 }
