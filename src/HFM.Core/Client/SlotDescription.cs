d7ed156b6787aa2f603a86c6265c2cd76650ab0c
harlam357 <harlam357@gmail.com>
2022-04-23 16:37:44 -0500

    modernize

--- src/HFM.Core/Client/SlotDescription.cs
+++ src/HFM.Core/Client/SlotDescription.cs
@@ -1,108 +1,104 @@
-﻿using System;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
-namespace HFM.Core.Client
+namespace HFM.Core.Client;
+
+public abstract class SlotDescription
 {
-    public abstract class SlotDescription
-    {
-        public abstract SlotType SlotType { get; }
+    public abstract SlotType SlotType { get; }
 
-        public string Value { get; set; }
+    public string Value { get; set; }
 
-        public static SlotDescription Parse(string value)
+    public static SlotDescription Parse(string value) =>
+        GetSlotType(value) switch
         {
-            switch (GetSlotType(value))
-            {
-                case SlotType.GPU:
-                    return GPUSlotDescription.Parse(value);
-                case SlotType.CPU:
-                    return CPUSlotDescription.Parse(value);
-                case SlotType.Unknown:
-                    break;
-            }
+            SlotType.GPU => GPUSlotDescription.Parse(value),
+            SlotType.CPU => CPUSlotDescription.Parse(value),
+            SlotType.Unknown => null,
+            _ => null
+        };
 
-            return null;
-        }
+    private static SlotType GetSlotType(string value) =>
+        String.IsNullOrWhiteSpace(value)
+            ? SlotType.Unknown
+            : value.StartsWith("gpu", StringComparison.OrdinalIgnoreCase)
+                ? SlotType.GPU
+                : SlotType.CPU;
+}
 
-        private static SlotType GetSlotType(string value) =>
-            String.IsNullOrWhiteSpace(value)
-                ? SlotType.Unknown
-                : value.StartsWith("gpu", StringComparison.OrdinalIgnoreCase)
-                    ? SlotType.GPU
-                    : SlotType.CPU;
+public class GPUSlotDescription : SlotDescription
+{
+    public override SlotType SlotType => SlotType.GPU;
+
+    public int? GPUBus { get; set; }
+    public int? GPUSlot { get; set; }
+    public string GPUPrefix { get; set; }
+    public string GPU { get; set; }
+
+    public static new GPUSlotDescription Parse(string value)
+    {
+        var d = new GPUSlotDescription
+        {
+            Value = value
+        };
+        d.SetGPUBusAndSlot(value);
+        d.SetGPUPrefix(value);
+        d.SetGPU(value);
+        return d;
     }
 
-    public class GPUSlotDescription : SlotDescription
+    private void SetGPUBusAndSlot(string description)
     {
-        public override SlotType SlotType => SlotType.GPU;
-
-        public int? GPUBus { get; set; }
-        public int? GPUSlot { get; set; }
-        public string GPUPrefix { get; set; }
-        public string GPU { get; set; }
-
-        public static new GPUSlotDescription Parse(string value)
+        var match = Regex.Match(description, @"gpu\:(?<GPUBus>\d+)\:(?<GPUSlot>\d+)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+        if (match.Success &&
+            Int32.TryParse(match.Groups["GPUBus"].Value, out var bus) &&
+            Int32.TryParse(match.Groups["GPUSlot"].Value, out var slot))
         {
-            var d = new GPUSlotDescription();
-            d.Value = value;
-            d.SetGPUBusAndSlot(value);
-            d.SetGPUPrefix(value);
-            d.SetGPU(value);
-            return d;
-        }
-
-        private void SetGPUBusAndSlot(string description)
-        {
-            var match = Regex.Match(description, @"gpu\:(?<GPUBus>\d+)\:(?<GPUSlot>\d+)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
-            if (match.Success &&
-                Int32.TryParse(match.Groups["GPUBus"].Value, out var bus) &&
-                Int32.TryParse(match.Groups["GPUSlot"].Value, out var slot))
-            {
-                GPUBus = bus;
-                GPUSlot = slot;
-            }
-        }
-
-        private void SetGPUPrefix(string description)
-        {
-            var match = Regex.Match(description, @"gpu\:\d+\:\d+ (?<GPUPrefix>.+) \[", RegexOptions.Singleline | RegexOptions.IgnoreCase);
-            if (match.Success)
-            {
-                GPUPrefix = match.Groups["GPUPrefix"].Value;
-            }
-        }
-
-        private void SetGPU(string description)
-        {
-            var match = Regex.Match(description, "\\[(?<GPU>.+)\\]", RegexOptions.Singleline);
-            if (match.Success)
-            {
-                GPU = match.Groups["GPU"].Value;
-            }
+            GPUBus = bus;
+            GPUSlot = slot;
         }
     }
 
-    public class CPUSlotDescription : SlotDescription
+    private void SetGPUPrefix(string description)
     {
-        public override SlotType SlotType => SlotType.CPU;
-
-        public int? CPUThreads { get; set; }
-
-        public static new CPUSlotDescription Parse(string value)
+        var match = Regex.Match(description, @"gpu\:\d+\:\d+ (?<GPUPrefix>.+) \[", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+        if (match.Success)
         {
-            var d = new CPUSlotDescription();
-            d.Value = value;
-            d.SetCPUThreads(value);
-            return d;
+            GPUPrefix = match.Groups["GPUPrefix"].Value;
         }
+    }
 
-        private void SetCPUThreads(string description)
+    private void SetGPU(string description)
+    {
+        var match = Regex.Match(description, "\\[(?<GPU>.+)\\]", RegexOptions.Singleline);
+        if (match.Success)
         {
-            var match = Regex.Match(description, @"[cpu|smp]\:(?<CPUThreads>\d+)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
-            if (match.Success && Int32.TryParse(match.Groups["CPUThreads"].Value, out var threads))
-            {
-                CPUThreads = threads;
-            }
+            GPU = match.Groups["GPU"].Value;
+        }
+    }
+}
+
+public class CPUSlotDescription : SlotDescription
+{
+    public override SlotType SlotType => SlotType.CPU;
+
+    public int? CPUThreads { get; set; }
+
+    public static new CPUSlotDescription Parse(string value)
+    {
+        var d = new CPUSlotDescription
+        {
+            Value = value
+        };
+        d.SetCPUThreads(value);
+        return d;
+    }
+
+    private void SetCPUThreads(string description)
+    {
+        var match = Regex.Match(description, @"[cpu|smp]\:(?<CPUThreads>\d+)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+        if (match.Success && Int32.TryParse(match.Groups["CPUThreads"].Value, out var threads))
+        {
+            CPUThreads = threads;
         }
     }
 }
