e09eb129b437abb766d561b531359b92ce28f2bc
harlam357 <harlam357@gmail.com>
2022-05-02 09:00:23 -0500

    Fix CSV serializer tests

--- src/HFM.Core.Tests/Data/WorkUnitRowCsvFileSerializerTests.cs
+++ src/HFM.Core.Tests/Data/WorkUnitRowCsvFileSerializerTests.cs
@@ -2,52 +2,51 @@
 
 using NUnit.Framework;
 
-namespace HFM.Core.Data
+namespace HFM.Core.Data;
+
+[TestFixture]
+public class WorkUnitRowCsvFileSerializerTests
 {
-    [TestFixture]
-    public class WorkUnitRowCsvFileSerializerTests
+    private List<WorkUnitRow> _value;
+    private string _expected;
+
+    [SetUp]
+    public void BeforeEach()
     {
-        private List<WorkUnitRow> _value;
-        private string _expected;
+        // Arrange
+        _value = new List<WorkUnitRow> { new() };
+        _expected = "DatabaseID,ProjectID,ProjectRun,ProjectClone,ProjectGen,Name,Path,Username,Team,CoreVersion,FramesCompleted,FrameTime,Result,Assigned,Finished,KFactor,Core,Frames,Atoms,PreferredDays,MaximumDays,SlotType,PPD,Credit,BaseCredit";
+        _expected += Environment.NewLine;
+        _expected += "0,0,0,0,0,,,,0,,0,00:00:00,,01/01/0001 00:00:00,01/01/0001 00:00:00,0,,0,0,0,0,,0,0,0";
+        _expected += Environment.NewLine;
+    }
 
-        [SetUp]
-        public void BeforeEach()
+    [Test]
+    public void WorkUnitRowCsvFileSerializer_Serialize_UsingCurrentCulture_Test()
+    {
+        // Act
+        string actual;
+        using (var writer = new StringWriter())
         {
-            // Arrange
-            _value = new List<WorkUnitRow> { new() };
-            _expected = "DatabaseID,ProjectID,ProjectRun,ProjectClone,ProjectGen,Name,Path,Username,Team,CoreVersion,FramesCompleted,FrameTime,Result,Assigned,Finished,WorkUnitName,KFactor,Core,Frames,Atoms,PreferredDays,MaximumDays,SlotType,PPD,Credit,BaseCredit";
-            _expected += Environment.NewLine;
-            _expected += "0,0,0,0,0,,,,0,,0,00:00:00,,01/01/0001 00:00:00,01/01/0001 00:00:00,,0,,0,0,0,0,,0,0,0";
-            _expected += Environment.NewLine;
+            WorkUnitRowCsvFileSerializer.Serialize(writer, _value);
+            actual = writer.ToString();
         }
+        Assert.AreEqual(_expected, actual);
+    }
 
-        [Test]
-        public void WorkUnitRowCsvFileSerializer_Serialize_UsingCurrentCulture_Test()
+    [Test]
+    public void WorkUnitRowCsvFileSerializer_Serialize_UsingCzechCulture_Test()
+    {
+        // Arrange
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("cs-CZ");
+        // Act
+        string actual;
+        using (var writer = new StringWriter())
         {
-            // Act
-            string actual;
-            using (var writer = new StringWriter())
-            {
-                WorkUnitRowCsvFileSerializer.Serialize(writer, _value);
-                actual = writer.ToString();
-            }
-            Assert.AreEqual(_expected, actual);
+            WorkUnitRowCsvFileSerializer.Serialize(writer, _value);
+            actual = writer.ToString();
         }
-
-        [Test]
-        public void WorkUnitRowCsvFileSerializer_Serialize_UsingCzechCulture_Test()
-        {
-            // Arrange
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("cs-CZ");
-            // Act
-            string actual;
-            using (var writer = new StringWriter())
-            {
-                WorkUnitRowCsvFileSerializer.Serialize(writer, _value);
-                actual = writer.ToString();
-            }
-            // Assert
-            Assert.AreEqual(_expected, actual);
-        }
+        // Assert
+        Assert.AreEqual(_expected, actual);
     }
 }
