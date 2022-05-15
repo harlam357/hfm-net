e642175ebc0753d714a000a1638abd7731887762
harlam357 <harlam357@gmail.com>
2022-05-15 12:30:36 -0500

    Modernize syntax

--- src/HFM.Forms/Models/ViewModelBase.cs
+++ src/HFM.Forms/Models/ViewModelBase.cs
@@ -1,37 +1,32 @@
-﻿
-using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace HFM.Forms.Models
+namespace HFM.Forms.Models;
+
+public abstract class ViewModelBase : INotifyPropertyChanged
 {
-    public abstract class ViewModelBase : INotifyPropertyChanged
+    public virtual string Error => String.Empty;
+
+    public virtual bool HasError => !String.IsNullOrWhiteSpace(Error);
+
+    public virtual bool ValidateAcceptance()
     {
-        public virtual string Error => String.Empty;
-
-        public virtual bool HasError => !String.IsNullOrWhiteSpace(Error);
-
-        public virtual bool ValidateAcceptance()
-        {
-            OnPropertyChanged(String.Empty);
-            return !HasError;
-        }
-
-        public virtual void Load()
-        {
-
-        }
-
-        public virtual void Save()
-        {
-
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        OnPropertyChanged(String.Empty);
+        return !HasError;
     }
+
+    public virtual void Load()
+    {
+
+    }
+
+    public virtual void Save()
+    {
+
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
--- src/HFM.Forms/Presenters/FormPresenter.cs
+++ src/HFM.Forms/Presenters/FormPresenter.cs
@@ -1,75 +1,66 @@
-﻿using System;
-
-using HFM.Forms.Models;
+﻿using HFM.Forms.Models;
 using HFM.Forms.Views;
 
-namespace HFM.Forms.Presenters
+namespace HFM.Forms.Presenters;
+
+public abstract class FormPresenter : IFormPresenter
 {
-    public abstract class FormPresenter : IFormPresenter
+    public virtual IWin32Form Form { get; protected set; }
+
+    public virtual void Show()
     {
-        public virtual IWin32Form Form { get; protected set; }
-
-        public virtual void Show()
-        {
-            Form = OnCreateForm();
-            Form.Closed += OnClosed;
-            Form.Show();
-        }
-
-        protected abstract IWin32Form OnCreateForm();
-
-        public virtual void Close()
-        {
-            Form?.Close();
-        }
-
-        public event EventHandler Closed;
-
-        protected virtual void OnClosed(object sender, EventArgs e)
-        {
-            Closed?.Invoke(this, e);
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        private bool _disposed;
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!_disposed)
-            {
-                if (disposing)
-                {
-                    Form?.Dispose();
-                }
-            }
-            _disposed = true;
-        }
+        Form = OnCreateForm();
+        Form.Closed += OnClosed;
+        Form.Show();
     }
 
-    public abstract class FormPresenter<TViewModel> : FormPresenter where TViewModel : ViewModelBase
+    protected abstract IWin32Form OnCreateForm();
+
+    public virtual void Close() => Form?.Close();
+
+    public event EventHandler Closed;
+
+    protected virtual void OnClosed(object sender, EventArgs e) => Closed?.Invoke(this, e);
+
+    public void Dispose()
     {
-        protected ViewModelBase ModelBase { get; }
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
 
-        protected FormPresenter(TViewModel model)
-        {
-            ModelBase = model;
-        }
+    private bool _disposed;
 
-        public override void Show()
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
         {
-            ModelBase.Load();
-            base.Show();
+            if (disposing)
+            {
+                Form?.Dispose();
+            }
         }
+        _disposed = true;
+    }
+}
 
-        protected override void OnClosed(object sender, EventArgs e)
-        {
-            ModelBase.Save();
-            base.OnClosed(sender, e);
-        }
+public abstract class FormPresenter<TViewModel> : FormPresenter where TViewModel : ViewModelBase
+{
+    protected ViewModelBase ModelBase { get; }
+
+    protected FormPresenter(TViewModel model)
+    {
+        ModelBase = model;
+    }
+
+    public override void Show()
+    {
+        ModelBase.Load();
+        base.Show();
+    }
+
+    protected override void OnClosed(object sender, EventArgs e)
+    {
+        ModelBase.Save();
+        base.OnClosed(sender, e);
     }
 }
