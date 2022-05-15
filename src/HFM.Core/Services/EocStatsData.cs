49d524540d9e843cd50ad1e71a4b570c8ec477c0
harlam357 <harlam357@gmail.com>
2022-05-15 17:23:07 -0500

    chore: EocStatsData uses a record
    record objects implement an implicit IEquatable

--- src/HFM.Core/Services/EocStatsData.cs
+++ src/HFM.Core/Services/EocStatsData.cs
@@ -3,202 +3,70 @@
 namespace HFM.Core.Services;
 
 [DataContract]
-public class EocStatsData : IEquatable<EocStatsData>
+public record EocStatsData
 {
-    public EocStatsData()
-    {
-
-    }
-
-    public EocStatsData(DateTime lastUpdated)
-    {
-        LastUpdated = lastUpdated;
-    }
-
-    /// <summary>
-    /// Stats Last Updated
-    /// </summary>
     [DataMember(Order = 1)]
     public DateTime LastUpdated { get; set; }
 
-    /// <summary>
-    /// User 24 Hour Points Average
-    /// </summary>
     [DataMember(Order = 2)]
     public long UserTwentyFourHourAverage { get; set; }
 
-    /// <summary>
-    /// User Points Today
-    /// </summary>
     [DataMember(Order = 3)]
     public long UserPointsToday { get; set; }
 
-    /// <summary>
-    /// User Points Week
-    /// </summary>
     [DataMember(Order = 4)]
     public long UserPointsWeek { get; set; }
 
-    /// <summary>
-    /// User Points Total
-    /// </summary>
     [DataMember(Order = 5)]
     public long UserPointsTotal { get; set; }
 
-    /// <summary>
-    /// User Work Units Total
-    /// </summary>
     [DataMember(Order = 6)]
     public long UserWorkUnitsTotal { get; set; }
 
-    /// <summary>
-    /// User Points Update
-    /// </summary>
     [DataMember(Order = 7)]
     public long UserPointsUpdate { get; set; }
 
-    /// <summary>
-    /// User Team Rank
-    /// </summary>
     [DataMember(Order = 8)]
     public int UserTeamRank { get; set; }
 
-    /// <summary>
-    /// User Overall Rank
-    /// </summary>
     [DataMember(Order = 9)]
     public int UserOverallRank { get; set; }
 
-    /// <summary>
-    /// User Change Rank Twenty Four Hours
-    /// </summary>
     [DataMember(Order = 10)]
     public int UserChangeRankTwentyFourHours { get; set; }
 
-    /// <summary>
-    /// User Change Rank Twenty Four Hours
-    /// </summary>
     [DataMember(Order = 11)]
     public int UserChangeRankSevenDays { get; set; }
 
-    /// <summary>
-    /// Team 24 Hour Points Average
-    /// </summary>
     [DataMember(Order = 12)]
     public long TeamTwentyFourHourAverage { get; set; }
 
-    /// <summary>
-    /// Team Points Today
-    /// </summary>
     [DataMember(Order = 13)]
     public long TeamPointsToday { get; set; }
 
-    /// <summary>
-    /// Team Points Week
-    /// </summary>
     [DataMember(Order = 14)]
     public long TeamPointsWeek { get; set; }
 
-    /// <summary>
-    /// Team Points Total
-    /// </summary>
     [DataMember(Order = 15)]
     public long TeamPointsTotal { get; set; }
 
-    /// <summary>
-    /// Team Work Units Total
-    /// </summary>
     [DataMember(Order = 16)]
     public long TeamWorkUnitsTotal { get; set; }
 
-    /// <summary>
-    /// Team Points Update
-    /// </summary>
     [DataMember(Order = 17)]
     public long TeamPointsUpdate { get; set; }
 
-    /// <summary>
-    /// Team Rank
-    /// </summary>
     [DataMember(Order = 18)]
     public int TeamRank { get; set; }
 
-    /// <summary>
-    /// Team Change Rank Twenty Four Hours
-    /// </summary>
     [DataMember(Order = 19)]
     public int TeamChangeRankTwentyFourHours { get; set; }
 
-    /// <summary>
-    /// Team Change Rank Twenty Four Hours
-    /// </summary>
     [DataMember(Order = 20)]
     public int TeamChangeRankSevenDays { get; set; }
 
     // not serialized
     public string Status { get; set; }
-
-    public bool Equals(EocStatsData other)
-    {
-        if (other is null) return false;
-        if (ReferenceEquals(this, other)) return true;
-        return UserTwentyFourHourAverage == other.UserTwentyFourHourAverage &&
-               UserPointsToday == other.UserPointsToday &&
-               UserPointsWeek == other.UserPointsWeek &&
-               UserPointsTotal == other.UserPointsTotal &&
-               UserWorkUnitsTotal == other.UserWorkUnitsTotal &&
-               UserPointsUpdate == other.UserPointsUpdate &&
-               UserTeamRank == other.UserTeamRank &&
-               UserOverallRank == other.UserOverallRank &&
-               UserChangeRankTwentyFourHours == other.UserChangeRankTwentyFourHours &&
-               UserChangeRankSevenDays == other.UserChangeRankSevenDays &&
-               TeamTwentyFourHourAverage == other.TeamTwentyFourHourAverage &&
-               TeamPointsToday == other.TeamPointsToday &&
-               TeamPointsWeek == other.TeamPointsWeek &&
-               TeamPointsTotal == other.TeamPointsTotal &&
-               TeamWorkUnitsTotal == other.TeamWorkUnitsTotal &&
-               TeamPointsUpdate == other.TeamPointsUpdate &&
-               TeamRank == other.TeamRank &&
-               TeamChangeRankTwentyFourHours == other.TeamChangeRankTwentyFourHours &&
-               TeamChangeRankSevenDays == other.TeamChangeRankSevenDays;
-    }
-
-    public override bool Equals(object obj)
-    {
-        if (obj is null) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != GetType()) return false;
-        return Equals((EocStatsData)obj);
-    }
-
-    public override int GetHashCode()
-    {
-        // ReSharper disable NonReadonlyMemberInGetHashCode
-        unchecked
-        {
-            var hashCode = UserTwentyFourHourAverage.GetHashCode();
-            hashCode = (hashCode * 397) ^ UserPointsToday.GetHashCode();
-            hashCode = (hashCode * 397) ^ UserPointsWeek.GetHashCode();
-            hashCode = (hashCode * 397) ^ UserPointsTotal.GetHashCode();
-            hashCode = (hashCode * 397) ^ UserWorkUnitsTotal.GetHashCode();
-            hashCode = (hashCode * 397) ^ UserPointsUpdate.GetHashCode();
-            hashCode = (hashCode * 397) ^ UserTeamRank;
-            hashCode = (hashCode * 397) ^ UserOverallRank;
-            hashCode = (hashCode * 397) ^ UserChangeRankTwentyFourHours;
-            hashCode = (hashCode * 397) ^ UserChangeRankSevenDays;
-            hashCode = (hashCode * 397) ^ TeamTwentyFourHourAverage.GetHashCode();
-            hashCode = (hashCode * 397) ^ TeamPointsToday.GetHashCode();
-            hashCode = (hashCode * 397) ^ TeamPointsWeek.GetHashCode();
-            hashCode = (hashCode * 397) ^ TeamPointsTotal.GetHashCode();
-            hashCode = (hashCode * 397) ^ TeamWorkUnitsTotal.GetHashCode();
-            hashCode = (hashCode * 397) ^ TeamPointsUpdate.GetHashCode();
-            hashCode = (hashCode * 397) ^ TeamRank;
-            hashCode = (hashCode * 397) ^ TeamChangeRankTwentyFourHours;
-            hashCode = (hashCode * 397) ^ TeamChangeRankSevenDays;
-            return hashCode;
-        }
-        // ReSharper restore NonReadonlyMemberInGetHashCode
-    }
 
     /// <summary>
     /// Gets the time for the next stats update time in UTC.
--- src/HFM.Core/Services/EocStatsService.cs
+++ src/HFM.Core/Services/EocStatsService.cs
@@ -1,89 +1,86 @@
-﻿using System;
-using System.Xml;
-using System.Xml.XPath;
+﻿using System.Xml;
 
 using HFM.Preferences;
 
-namespace HFM.Core.Services
+namespace HFM.Core.Services;
+
+public interface IEocStatsService
 {
-    public interface IEocStatsService
+    EocStatsData GetStatsData();
+}
+
+public class EocStatsService : IEocStatsService
+{
+    public const string UserBaseUrl = "https://folding.extremeoverclocking.com/user_summary.php?s=&u=";
+    public const string TeamBaseUrl = "https://folding.extremeoverclocking.com/team_summary.php?s=&t=";
+    public const string UserXmlBaseUrl = "https://folding.extremeoverclocking.com/xml/user_summary.php?u=";
+
+    private readonly IPreferences _preferences;
+
+    public EocStatsService(IPreferences preferences)
     {
-        EocStatsData GetStatsData();
+        _preferences = preferences;
     }
 
-    public class EocStatsService : IEocStatsService
+    public EocStatsData GetStatsData()
     {
-        public const string UserBaseUrl = "https://folding.extremeoverclocking.com/user_summary.php?s=&u=";
-        public const string TeamBaseUrl = "https://folding.extremeoverclocking.com/team_summary.php?s=&t=";
-        public const string UserXmlBaseUrl = "https://folding.extremeoverclocking.com/xml/user_summary.php?u=";
+        string eocXmlDataUrl = String.Concat(UserXmlBaseUrl, _preferences.Get<int>(Preference.EocUserId));
 
-        private readonly IPreferences _preferences;
-
-        public EocStatsService(IPreferences preferences)
+        var xmlData = new XmlDocument();
+        using (var reader = XmlReader.Create(eocXmlDataUrl, new XmlReaderSettings { XmlResolver = null }))
         {
-            _preferences = preferences;
+            xmlData.Load(reader);
+        }
+        xmlData.RemoveChild(xmlData.ChildNodes[0]);
+
+        XmlNode eocNode = GetXmlNode(xmlData, "EOC_Folding_Stats");
+        return CreateStatsDataFromXmlNode(eocNode);
+    }
+
+    /// <summary>
+    /// Updates the data container
+    /// </summary>
+    /// <param name="eocNode">EOC Stats XmlNode</param>
+    private static EocStatsData CreateStatsDataFromXmlNode(XmlNode eocNode)
+    {
+        XmlNode teamNode = eocNode.SelectSingleNode("team");
+        XmlNode userNode = eocNode.SelectSingleNode("user");
+
+        var data = new EocStatsData { LastUpdated = DateTime.UtcNow };
+        data.UserTwentyFourHourAverage = Convert.ToInt64(GetXmlNode(userNode, "Points_24hr_Avg").InnerText);
+        data.UserPointsToday = Convert.ToInt64(GetXmlNode(userNode, "Points_Today").InnerText);
+        data.UserPointsWeek = Convert.ToInt64(GetXmlNode(userNode, "Points_Week").InnerText);
+        data.UserPointsTotal = Convert.ToInt64(GetXmlNode(userNode, "Points").InnerText);
+        data.UserWorkUnitsTotal = Convert.ToInt64(GetXmlNode(userNode, "WUs").InnerText);
+        data.UserPointsUpdate = Convert.ToInt64(GetXmlNode(userNode, "Points_Update").InnerText);
+        data.UserTeamRank = Convert.ToInt32(GetXmlNode(userNode, "Team_Rank").InnerText);
+        data.UserOverallRank = Convert.ToInt32(GetXmlNode(userNode, "Overall_Rank").InnerText);
+        data.UserChangeRankTwentyFourHours = Convert.ToInt32(GetXmlNode(userNode, "Change_Rank_24hr").InnerText);
+        data.UserChangeRankSevenDays = Convert.ToInt32(GetXmlNode(userNode, "Change_Rank_7days").InnerText);
+        data.TeamTwentyFourHourAverage = Convert.ToInt64(GetXmlNode(teamNode, "Points_24hr_Avg").InnerText);
+        data.TeamPointsToday = Convert.ToInt64(GetXmlNode(teamNode, "Points_Today").InnerText);
+        data.TeamPointsWeek = Convert.ToInt64(GetXmlNode(teamNode, "Points_Week").InnerText);
+        data.TeamPointsTotal = Convert.ToInt64(GetXmlNode(teamNode, "Points").InnerText);
+        data.TeamWorkUnitsTotal = Convert.ToInt64(GetXmlNode(teamNode, "WUs").InnerText);
+        data.TeamPointsUpdate = Convert.ToInt64(GetXmlNode(teamNode, "Points_Update").InnerText);
+        data.TeamRank = Convert.ToInt32(GetXmlNode(teamNode, "Rank").InnerText);
+        data.TeamChangeRankTwentyFourHours = Convert.ToInt32(GetXmlNode(teamNode, "Change_Rank_24hr").InnerText);
+        data.TeamChangeRankSevenDays = Convert.ToInt32(GetXmlNode(teamNode, "Change_Rank_7days").InnerText);
+
+        XmlNode statusNode = GetXmlNode(eocNode, "status");
+        data.Status = GetXmlNode(statusNode, "Update_Status").InnerText;
+
+        return data;
+    }
+
+    private static XmlNode GetXmlNode(XmlNode xmlNode, string xpath)
+    {
+        XmlNode node = xmlNode.SelectSingleNode(xpath);
+        if (node == null)
+        {
+            throw new XmlException($"Node '{xpath}' does not exist.");
         }
 
-        public EocStatsData GetStatsData()
-        {
-            string eocXmlDataUrl = String.Concat(UserXmlBaseUrl, _preferences.Get<int>(Preference.EocUserId));
-
-            var xmlData = new XmlDocument();
-            using (var reader = XmlReader.Create(eocXmlDataUrl, new XmlReaderSettings { XmlResolver = null }))
-            {
-                xmlData.Load(reader);
-            }
-            xmlData.RemoveChild(xmlData.ChildNodes[0]);
-
-            XmlNode eocNode = GetXmlNode(xmlData, "EOC_Folding_Stats");
-            return CreateStatsDataFromXmlNode(eocNode);
-        }
-
-        /// <summary>
-        /// Updates the data container
-        /// </summary>
-        /// <param name="eocNode">EOC Stats XmlNode</param>
-        private static EocStatsData CreateStatsDataFromXmlNode(XmlNode eocNode)
-        {
-            XmlNode teamNode = eocNode.SelectSingleNode("team");
-            XmlNode userNode = eocNode.SelectSingleNode("user");
-
-            var data = new EocStatsData(DateTime.UtcNow);
-            data.UserTwentyFourHourAverage = Convert.ToInt64(GetXmlNode(userNode, "Points_24hr_Avg").InnerText);
-            data.UserPointsToday = Convert.ToInt64(GetXmlNode(userNode, "Points_Today").InnerText);
-            data.UserPointsWeek = Convert.ToInt64(GetXmlNode(userNode, "Points_Week").InnerText);
-            data.UserPointsTotal = Convert.ToInt64(GetXmlNode(userNode, "Points").InnerText);
-            data.UserWorkUnitsTotal = Convert.ToInt64(GetXmlNode(userNode, "WUs").InnerText);
-            data.UserPointsUpdate = Convert.ToInt64(GetXmlNode(userNode, "Points_Update").InnerText);
-            data.UserTeamRank = Convert.ToInt32(GetXmlNode(userNode, "Team_Rank").InnerText);
-            data.UserOverallRank = Convert.ToInt32(GetXmlNode(userNode, "Overall_Rank").InnerText);
-            data.UserChangeRankTwentyFourHours = Convert.ToInt32(GetXmlNode(userNode, "Change_Rank_24hr").InnerText);
-            data.UserChangeRankSevenDays = Convert.ToInt32(GetXmlNode(userNode, "Change_Rank_7days").InnerText);
-            data.TeamTwentyFourHourAverage = Convert.ToInt64(GetXmlNode(teamNode, "Points_24hr_Avg").InnerText);
-            data.TeamPointsToday = Convert.ToInt64(GetXmlNode(teamNode, "Points_Today").InnerText);
-            data.TeamPointsWeek = Convert.ToInt64(GetXmlNode(teamNode, "Points_Week").InnerText);
-            data.TeamPointsTotal = Convert.ToInt64(GetXmlNode(teamNode, "Points").InnerText);
-            data.TeamWorkUnitsTotal = Convert.ToInt64(GetXmlNode(teamNode, "WUs").InnerText);
-            data.TeamPointsUpdate = Convert.ToInt64(GetXmlNode(teamNode, "Points_Update").InnerText);
-            data.TeamRank = Convert.ToInt32(GetXmlNode(teamNode, "Rank").InnerText);
-            data.TeamChangeRankTwentyFourHours = Convert.ToInt32(GetXmlNode(teamNode, "Change_Rank_24hr").InnerText);
-            data.TeamChangeRankSevenDays = Convert.ToInt32(GetXmlNode(teamNode, "Change_Rank_7days").InnerText);
-
-            XmlNode statusNode = GetXmlNode(eocNode, "status");
-            data.Status = GetXmlNode(statusNode, "Update_Status").InnerText;
-
-            return data;
-        }
-
-        private static XmlNode GetXmlNode(XmlNode xmlNode, string xpath)
-        {
-            XmlNode node = xmlNode.SelectSingleNode(xpath);
-            if (node == null)
-            {
-                throw new XmlException($"Node '{xpath}' does not exist.");
-            }
-
-            return node;
-        }
+        return node;
     }
 }
