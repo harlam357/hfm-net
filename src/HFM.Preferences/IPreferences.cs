042da12ec97d0f023815544192e30d1d2f2442c6
harlam357 <harlam357@gmail.com>
2022-05-15 18:36:12 -0500

    clean: CA1004 suppress attribute

--- src/HFM.Preferences/IPreferences.cs
+++ src/HFM.Preferences/IPreferences.cs
@@ -1,54 +1,49 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿namespace HFM.Preferences;
 
-namespace HFM.Preferences
+public interface IPreferences
 {
-    public interface IPreferences
+    /// <summary>
+    /// Resets the preferences to default values.
+    /// </summary>
+    void Reset();
+
+    /// <summary>
+    /// Loads the preferences from the last saved values.
+    /// </summary>
+    void Load();
+
+    /// <summary>
+    /// Saves the preferences.
+    /// </summary>
+    void Save();
+
+    /// <summary>
+    /// Gets a preference value.
+    /// </summary>
+    /// <typeparam name="T">The type of the preference value.</typeparam>
+    /// <param name="key">The preference key.</param>
+    T Get<T>(Preference key);
+
+    /// <summary>
+    /// Sets a preference value.
+    /// </summary>
+    /// <typeparam name="T">The type of the preference value.</typeparam>
+    /// <param name="key">The preference key.</param>
+    /// <param name="value">The preference value.</param>
+    void Set<T>(Preference key, T value);
+
+    /// <summary>
+    /// Raised when a preference value is changed.
+    /// </summary>
+    event EventHandler<PreferenceChangedEventArgs> PreferenceChanged;
+}
+
+public class PreferenceChangedEventArgs : EventArgs
+{
+    public Preference Preference { get; }
+
+    public PreferenceChangedEventArgs(Preference preference)
     {
-        /// <summary>
-        /// Resets the preferences to default values.
-        /// </summary>
-        void Reset();
-
-        /// <summary>
-        /// Loads the preferences from the last saved values.
-        /// </summary>
-        void Load();
-
-        /// <summary>
-        /// Saves the preferences.
-        /// </summary>
-        void Save();
-
-        /// <summary>
-        /// Gets a preference value.
-        /// </summary>
-        /// <typeparam name="T">The type of the preference value.</typeparam>
-        /// <param name="key">The preference key.</param>
-        [SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter")]
-        T Get<T>(Preference key);
-
-        /// <summary>
-        /// Sets a preference value.
-        /// </summary>
-        /// <typeparam name="T">The type of the preference value.</typeparam>
-        /// <param name="key">The preference key.</param>
-        /// <param name="value">The preference value.</param>
-        void Set<T>(Preference key, T value);
-
-        /// <summary>
-        /// Raised when a preference value is changed.
-        /// </summary>
-        event EventHandler<PreferenceChangedEventArgs> PreferenceChanged;
-    }
-
-    public class PreferenceChangedEventArgs : EventArgs
-    {
-        public Preference Preference { get; }
-
-        public PreferenceChangedEventArgs(Preference preference)
-        {
-            Preference = preference;
-        }
+        Preference = preference;
     }
 }
