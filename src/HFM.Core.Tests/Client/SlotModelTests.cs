f1fcf925247f14ecdc9cf9679d1391449a4b4e4c
harlam357 <harlam357@gmail.com>
2022-05-30 12:31:48 -0500

    refactor: consolidate SlotModelTests

--- src/HFM.Core.Tests/Client/SlotModelTests.cs
+++ src/HFM.Core.Tests/Client/SlotModelTests.cs
@@ -1,4 +1,6 @@
-﻿using HFM.Core.WorkUnits;
+﻿using HFM.Core.Client.Mocks;
+using HFM.Core.WorkUnits;
+using HFM.Log;
 using HFM.Preferences;
 
 using NUnit.Framework;
@@ -36,6 +38,51 @@ public class SlotModelTests
         {
             var source = (IProteinBenchmarkDetailSource)_slotModel;
             Assert.AreEqual("GeForce RTX 3070 Ti", source.Processor);
+        }
+    }
+
+    [TestFixture]
+    public class SlotModelCurrentLogLines
+    {
+        [Test]
+        public void IsThreadSafe()
+        {
+            var slot = new SlotModel(new MockClient());
+            var random = new Random();
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+            var token = cts.Token;
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    slot.CurrentLogLines = Enumerable.Repeat(new LogLine(), random.Next(1, 5)).ToList();
+                    if (token.IsCancellationRequested)
+                    {
+                        break;
+                    }
+                }
+            }, token);
+
+            const int count = 100;
+
+            var tasks = Enumerable.Range(0, count)
+                .Select(i => Task.Run(() =>
+                {
+                    Thread.Sleep(10);
+                    _ = slot.CurrentLogLines.ToList();
+                }))
+                .ToArray();
+
+            try
+            {
+                Task.WaitAll(tasks);
+                cts.Cancel();
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Enumeration failed");
+            }
         }
     }
 }
