9d7dbd586d5ffc84b42ba5ffd72e016446ce95a5
harlam357 <harlam357@gmail.com>
2022-11-25 16:55:54 -0600

    clean: modernize syntax

--- src/HFM.Preferences/XmlPreferencesProvider.PreferenceUpgrade.cs
+++ src/HFM.Preferences/XmlPreferencesProvider.PreferenceUpgrade.cs
@@ -1,62 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿namespace HFM.Preferences;
 
-namespace HFM.Preferences
+public partial class XmlPreferencesProvider
 {
-    public partial class XmlPreferencesProvider
+    protected override IEnumerable<PreferenceUpgrade> OnEnumerateUpgrades()
     {
-        protected override IEnumerable<PreferenceUpgrade> OnEnumerateUpgrades()
+        yield return new PreferenceUpgrade
         {
-            yield return new PreferenceUpgrade
-            {
-                Version = new Version(0, 9, 13),
-                Action = data => data.ApplicationSettings.ProjectDownloadUrl = Data.ApplicationSettings.DefaultProjectDownloadUrl
-            };
+            Version = new Version(0, 9, 13),
+            Action = data => data.ApplicationSettings.ProjectDownloadUrl = Data.ApplicationSettings.DefaultProjectDownloadUrl
+        };
 
-            yield return new PreferenceUpgrade
-            {
-                Version = new Version(9, 25),
-                Action = data => data.MainWindowGrid.Columns = InsertGridColumn(data.MainWindowGrid.Columns, 4)
-            };
-
-            yield return new PreferenceUpgrade
-            {
-                Version = new Version(9, 26),
-                Action = data => data.MainWindowState.QueueSplitterLocation = Data.MainWindowState.DefaultQueueSplitterLocation
-            };
-        }
-
-        private static List<string> InsertGridColumn(IEnumerable<string> columns, int insertIndex)
+        yield return new PreferenceUpgrade
         {
-            if (columns is null) return null;
+            Version = new Version(9, 25),
+            Action = data => data.MainWindowGrid.Columns = InsertGridColumn(data.MainWindowGrid.Columns, 4)
+        };
 
-            var parsed = columns
-                .Select(FormColumnPreference.Parse)
-                .Where(x => x.HasValue)
-                .Select(x => x.Value);
+        yield return new PreferenceUpgrade
+        {
+            Version = new Version(9, 26),
+            Action = data => data.MainWindowState.QueueSplitterLocation = Data.MainWindowState.DefaultQueueSplitterLocation
+        };
+    }
 
-            var result = new List<string>();
+    private static List<string> InsertGridColumn(IEnumerable<string> columns, int insertIndex)
+    {
+        if (columns is null) return null;
 
-            foreach (var c in parsed)
+        var parsed = columns
+            .Select(FormColumnPreference.Parse)
+            .Where(x => x.HasValue)
+            .Select(x => x.Value);
+
+        var result = new List<string>();
+
+        foreach (var c in parsed)
+        {
+            int displayIndex = c.DisplayIndex;
+            int index = c.Index;
+
+            if (displayIndex >= insertIndex)
             {
-                int displayIndex = c.DisplayIndex;
-                int index = c.Index;
-
-                if (displayIndex >= insertIndex)
-                {
-                    displayIndex += 1;
-                }
-
-                if (index >= insertIndex)
-                {
-                    index += 1;
-                }
-
-                result.Add(FormColumnPreference.Format(displayIndex, c.Width, c.Visible, index));
+                displayIndex += 1;
             }
 
-            return result;
+            if (index >= insertIndex)
+            {
+                index += 1;
+            }
+
+            result.Add(FormColumnPreference.Format(displayIndex, c.Width, c.Visible, index));
         }
+
+        return result;
     }
 }
